0eb5eb96d446d9e892f5a146916a4c41a9728603
marcel2bade@gmail.com <k?M24#!!>
2022-04-04 15:34:28 +0200

    rerworked  talbe components

---
+++ src/components/compendiums/factionTable/components/detailedCardView.js
@@ -0,0 +1,30 @@
+// React
+import React from "react";
+// Material UI
+import { makeStyles } from "@material-ui/core/styles";
+// components & functions
+import { uuidGenerator } from "../../../shared/sharedFunctions";
+import { unitOrCmdCard } from "../depencies/factionTableFunctions";
+
+const useStyles = makeStyles({
+  backGround: {
+    backgroundColor: "white",
+    "& :hover": {
+      backgroundColor: "white",
+    },
+  },
+});
+
+const DetailedCardView = (props) => {
+ const classes = useStyles();
+
+  return (
+    <tr key={uuidGenerator()} className={classes.backGround}>
+      <td key={uuidGenerator()} colSpan="100%">
+        {props.selectedCards.includes(props.unit.faction + props.unit.name) ? unitOrCmdCard(props.unit) : null}
+      </td>
+    </tr>
+  );
+};
+
+export default React.memo(DetailedCardView);
