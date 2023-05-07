95dae7628814ebd502a4d31142fb825d67670719
marcelbade <62931394+marcelbade@users.noreply.github.com>
2023-05-07 10:19:30 +0200

    removed imports

--- src/components/ListGenerator/ItemShop/ItemShop.js
+++ src/components/ListGenerator/ItemShop/ItemShop.js
@@ -3,8 +3,6 @@ import React, { useState, useContext, useEffect } from "react";
 //Material UI
 import { Button, Grid, ButtonGroup, Typography, Accordion, AccordionSummary, AccordionDetails } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import { IconButton } from "@material-ui/core";
-import CloseIcon from "@material-ui/icons/Close";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 // components and functions
 import { ArmyContext } from "../../../contexts/armyContext";
