74a1ff2133ec5fd16c6b23de8cccbd8a8fc38421
marcelbade <62931394+marcelbade@users.noreply.github.com>
2023-05-10 12:51:35 +0200

    Removed dead code

--- src/components/ListGenerator/ItemShop/ItemShopPanel.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const ItemShopPanel = (props) => {
-  const { children, value, index } = props;
-  return <div>{value === index && <h1>{children}</h1>}</div>;
-};
-
-export default ItemShopPanel;
