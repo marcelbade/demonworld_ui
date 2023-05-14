21e684aa5b87b6711ef02c4c00ff705fd30a9e4f
marcelbade <62931394+marcelbade@users.noreply.github.com>
2023-05-14 11:28:54 +0200

    added faction constants

--- src/constants/factions.js
+++ src/constants/factions.js
@@ -9,15 +9,20 @@ export const ELFEN = "Elfen";
 export const NORWINGER = "Norwinger";
 export const UNTOTE = "Untote";
 
-export const ALL_FACTIONS_ARRAY = [
-  DUNKELFELFEN,
-  ELFEN,
-  GOBLINS,
-  IMPERIUM,
-  ISHTAK,
-  NORWINGER,
-  ORKS,
-  THAIN,
-  UNTOTE,
-  ZWERGE,
-];
+export const ALL_FACTIONS_ARRAY = [DUNKELFELFEN, ELFEN, GOBLINS, IMPERIUM, ISHTAK, NORWINGER, ORKS, THAIN, UNTOTE, ZWERGE];
+
+export const ARMIES_WITH_ALTERNATIVE_LISTS = ["Imperium", "Zwerge", "Orks"];
+
+export const ARMY_TO_ALTERNATIVES_MAPPER = {
+  Imperium: ["Nordmark", "Südmark", "Westmark", "Ostmark"],
+  Zwerge: ["Gaeta", "Zah'ra", "Imperium"],
+  Orks: ["Clanngetts", "Steinclan", "Wyvernclan", "Tierclan", "Eisclan", "Pfeilclan", "Blutclan", "Eisenclan", "Bergclan"],
+};
+
+
+export const ALTERNATIVE_ARMY_SELECTION_TEXT = {
+  Imperium: "Wähle eine der Marken",
+  Zwerge: "Wähle eines der Königreiche oder das Imperium als Alliierte",
+  Orks: "Wähle einen der Clans oder Clanngett",  
+}
+ 
