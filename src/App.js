62d68b4955403ec08a7fc1659fc0a4721816684f
marcelbade <62931394+marcelbade@users.noreply.github.com>
2023-05-19 12:54:35 +0200

    Landing page: remade and made responsive

--- src/App.js
+++ src/App.js
@@ -1,131 +1,35 @@
 // React
 import * as React from "react";
+
 // Material UI
-import {
-  ThemeProvider,
-  createMuiTheme,
-  Drawer,
-  List,
-  IconButton,
-  ListItem,
-  ListItemIcon,
-  Box,
-  CssBaseline,
-  styled,
-  Grid,
-} from "@material-ui/core";
+import { Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
 // router
-import { Route, Switch, Link } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 // components and functions
 import landingPage from "./components/landingPage/landingPage";
 import factionTable from "./components/compendiums/factionTable/components/factionTable";
 import ListGeneratorController from "./components/ListGenerator/ListGeneratorController";
-// icons
-import HomeIcon from "@material-ui/icons/Home";
-import MenuBookIcon from "@material-ui/icons/MenuBook";
-import ChevronLeftIcon from "@material-ui/icons/ArrowBackIos";
-import MenuIcon from "@material-ui/icons/Menu";
+import LossCalculator from "./components/lossCalculator/LossCalculator";
 
-const theme = createMuiTheme();
-
-const DRAWER_WIDTH = 240;
-
-const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(({ theme, open }) => ({
-  flexGrow: 1,
-  paddingLeft: theme.spacing(30),
-  transition: theme.transitions.create("margin", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  marginLeft: `-${DRAWER_WIDTH}px`,
-  ...(open && {
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-    marginLeft: 0,
-  }),
+const useStyles = makeStyles((theme) => ({
+  homePage: {
+    backgroundColor: "pink",
+  },
 }));
-
-const DrawerHeader = styled("div")(({ theme }) => ({
-  display: "flex",
-  alignItems: "center",
-  padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
-  ...theme.mixins.toolbar,
-  justifyContent: "flex-end",
-}));
-
-// correct Header
 
 function App() {
-  //const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
-
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
-
+  const classes = useStyles();
   return (
-    <ThemeProvider theme={theme}>
-      <Box sx={{ display: "flex" }}>
-        <CssBaseline />
-        <Drawer
-          sx={{
-            width: DRAWER_WIDTH,
-            flexShrink: 0,
-            "& .MuiDrawer-paper": {
-              width: DRAWER_WIDTH,
-              boxSizing: "border-box",
-            },
-          }}
-          variant="persistent"
-          anchor="left"
-          open={open}
-        >
-          <DrawerHeader>
-            <IconButton onClick={handleDrawerClose}>
-              <ChevronLeftIcon />
-            </IconButton>
-          </DrawerHeader>
-          <List>
-            <ListItem>
-              <ListItemIcon>
-                <HomeIcon />
-              </ListItemIcon>
-              <Link to="/">Startseite</Link>
-            </ListItem>
-            <ListItem>
-              <ListItemIcon>
-                <MenuBookIcon />
-              </ListItemIcon>
-              <Link to="/compendium">Kompendium</Link>
-            </ListItem>
-            <ListItem>
-              <ListItemIcon></ListItemIcon>
-              <Link to="/listGenerator">Armeegenerator</Link>
-            </ListItem>
-          </List>
-        </Drawer>
-        <Main open={open}>
-          <DrawerHeader />
-          <Grid container>
-            <IconButton aria-label="open drawer" onClick={handleDrawerOpen}>
-              {!open ? <MenuIcon /> : null}
-            </IconButton>
-            <Switch>
-              <Route path="/" component={landingPage} exact />
-              <Route path="/compendium" component={factionTable} exact />
-              <Route path="/listGenerator" component={ListGeneratorController} />
-            </Switch>
-          </Grid>
-        </Main>
-      </Box>
-    </ThemeProvider>
+    <Grid container>
+      <Switch>
+        <Route path="/" component={landingPage} exact />
+        <Route path="/compendium" component={factionTable} exact />
+        <Route path="/listGenerator" component={ListGeneratorController} />
+        <Route path="/lossCalculator" component={LossCalculator} />
+      </Switch>
+    </Grid>
   );
 }
 
--- src/components/landingPage/landingPage.js
+++ src/components/landingPage/landingPage.js
@@ -1,19 +1,108 @@
 // React
 import React from "react";
+import { Link } from "react-router-dom";
 // Material UI
-// import { makeStyles } from "@material-ui/core/styles";
+import { IconButton, Grid, Tooltip, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { styled } from "@mui/system";
 
-// const useStyles = makeStyles({
-//   placeholder: {
-//     color: "black",
-//     fontSize: "20 em",
-//   },
-// });
+// icons
+import ChevronLeftIcon from "@material-ui/icons/ArrowBackIos";
+import deathIcon from "../../icons/icons8-death-64.png";
+import calculatorIcon from "../../icons/icons8-calculator-64.png";
+import bookIcon from "../../icons/icons8-book-64.png";
+
+const useStyles = makeStyles((theme) => ({
+  homePage: {
+    width: "100vw",
+    height: "100vh",
+
+    [theme.breakpoints.up("md")]: {
+      flexDirection: "row",
+    },
+    [theme.breakpoints.down("md")]: {
+      flexDirection: "column",
+      "@media (orientation:landscape)": {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        padding: "1em",
+      },
+    },
+  },
+
+  iconContainer: {
+    width: "20em",
+    height: "20em",
+    textAlign: "center",
+
+    [theme.breakpoints.up("md")]: {
+      width: "20em",
+      height: "20em",
+    },
+    [theme.breakpoints.down("md")]: {
+      width: "10em",
+      height: "10em",
+    },
+  },
+  mobileToolTip: {
+    fontFamily: "NotMaryKate",
+    fontSize: "15px",
+    [theme.breakpoints.up("md")]: {
+      display: "none",
+    },
+    [theme.breakpoints.down("md")]: {},
+  },
+
+  tooltipText: {
+    fontFamily: "NotMaryKate",
+    fontSize: "20px",
+  },
+}));
+
+const CustomToolTip = styled((props) => <Tooltip classes={{ popper: props.className }} {...props} />)`
+  & .MuiTooltip-tooltip {
+    width: 1000px;
+    height: 200px;
+    background-color: white;
+    font-family: NotMaryKate; 
+    color:   black;
+    .tooltip: {
+      fonz-size: 50px; 
+  },
+  }
+`;
 
 const LandingPage = (props) => {
-  // const classes = useStyles();
+  const classes = useStyles();
 
-  return <p className="classes.placeholder">PLACEHOLDER!</p>;
+  return (
+    <Grid container justify="center" alignContent="center" className={classes.homePage}>
+      <Grid item className={classes.iconContainer}>
+        <Tooltip title={<Typography className={classes.tooltipText}>Kompendium</Typography>}>
+          <IconButton component={Link} to="/compendium">
+            <img src={bookIcon} height={100} width={100} />
+          </IconButton>
+        </Tooltip>
+        <Typography className={classes.mobileToolTip}>compendium</Typography>
+      </Grid>
+      <Grid item className={classes.iconContainer}>
+        <Tooltip title={<Typography className={classes.tooltipText}>Armeelistengenerator</Typography>}>
+          <IconButton component={Link} to="/listGenerator">
+            <img src={calculatorIcon} height={100} width={100} />
+          </IconButton>
+        </Tooltip>
+        <Typography className={classes.mobileToolTip}>Armeelistengenerator</Typography>
+      </Grid>
+      <Grid item className={classes.iconContainer}>
+        <Tooltip title={<Typography className={classes.tooltipText}>Verlustrechner</Typography>}>
+          <IconButton component={Link} to="/lossCalculator">
+            <img src={deathIcon} height={100} width={100} />
+          </IconButton>
+        </Tooltip>
+        <Typography className={classes.mobileToolTip}>Verlustrechner</Typography>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default LandingPage;
