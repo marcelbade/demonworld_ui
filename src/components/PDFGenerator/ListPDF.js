c8a85a5d46a3c411c2983e33f9be77774963306d
marcelbade <62931394+marcelbade@users.noreply.github.com>
2023-05-28 23:35:21 +0200

    removed dead code

--- src/components/PDFGenerator/ListPDF.js
+++ src/components/PDFGenerator/ListPDF.js
@@ -1,26 +1,25 @@
 import React from "react";
 import { Page, Text, View, Document, Font } from "@react-pdf/renderer";
 import notMaryKate from "../../fonts/notMaryKate.ttf";
+import Beryliumbold from "../../fonts/Beryliumbold.ttf";
 
 // Register font
 Font.register({ family: "notMaryKate", src: notMaryKate });
+Font.register({ family: "Beryliumbold", src: Beryliumbold });
 
 // Create Document Component
 const ListPDF = (props) => {
-  // let units = props.units;
-  // let distinctSubFactions = props.distinctSubFactions;
-
   const DynamicList = () => {
     return props.pdfMasterList
       .filter((obj) => obj.units.length > 0)
       .map((obj) => (
         <View style={{ marginLeft: 15, marginTop: 8 }}>
-          <Text style={{ textAlign: "left", fontFamily: "notMaryKate", fontSize: 10 }}>
+          <Text style={{ textAlign: "left", fontFamily: "notMaryKate", fontSize: 15 }}>
             {/* SUBFACTION */}
             {obj.subFaction}
           </Text>
           {obj.units.map((u) => (
-            <Text style={{ textAlign: "left", fontFamily: "notMaryKate", fontSize: 10 }}>
+            <Text style={{ textAlign: "left", fontFamily: "Beryliumbold", fontSize: 10 }}>
               {/* UNIT */}
               {u.unitName} - {u.points}
               {u.equipment.length > 0 ? (
@@ -36,7 +35,7 @@ const ListPDF = (props) => {
               {u.equipment.length > 0
                 ? u.equipment.map((e) => (
                     <View style={{ marginLeft: 35 }}>
-                      <Text style={{ textAlign: "left", fontFamily: "notMaryKate", fontSize: 10 }}>
+                      <Text style={{ textAlign: "left", fontFamily: "Beryliumbold", fontSize: 10 }}>
                         {/* EQUIPMENT */}
                         {e.name} - {e.points}
                       </Text>
