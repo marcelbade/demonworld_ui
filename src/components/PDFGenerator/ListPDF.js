74fd49c8ee5ad05981461bbd0cb85c7da61db3c6
marcelbade <62931394+marcelbade@users.noreply.github.com>
2023-05-28 23:23:40 +0200

    finished pdf logic for army list render

--- src/components/PDFGenerator/ListPDF.js
+++ src/components/PDFGenerator/ListPDF.js
@@ -7,36 +7,46 @@ Font.register({ family: "notMaryKate", src: notMaryKate });
 
 // Create Document Component
 const ListPDF = (props) => {
-  let units = props.units;
+  // let units = props.units;
   // let distinctSubFactions = props.distinctSubFactions;
 
   const DynamicList = () => {
-    return units.map((u) => (
-      <View style={{ marginLeft: 15, marginTop: 8 }}>
-        <Text style={{ textAlign: "left", fontFamily: "notMaryKate", fontSize: 10 }}>
-          {u.unitName} - {u.points}
-        </Text>
-        {u.equipment.length > 0 ? (
-          <Text
-            style={{
-              marginRight: 400,
-              borderBottomColor: "black",
-              borderBottomWidth: 1,
-              marginBorder: 2,
-            }}
-          ></Text>
-        ) : null}
-        {u.equipment.length > 0
-          ? u.equipment.map((e) => (
-              <View style={{ marginLeft: 35 }}>
-                <Text style={{ textAlign: "left", fontFamily: "notMaryKate", fontSize: 10 }}>
-                  {e.name} - {e.points}
-                </Text>
-              </View>
-            ))
-          : null}
-      </View>
-    ));
+    return props.pdfMasterList
+      .filter((obj) => obj.units.length > 0)
+      .map((obj) => (
+        <View style={{ marginLeft: 15, marginTop: 8 }}>
+          <Text style={{ textAlign: "left", fontFamily: "notMaryKate", fontSize: 10 }}>
+            {/* SUBFACTION */}
+            {obj.subFaction}
+          </Text>
+          {obj.units.map((u) => (
+            <Text style={{ textAlign: "left", fontFamily: "notMaryKate", fontSize: 10 }}>
+              {/* UNIT */}
+              {u.unitName} - {u.points}
+              {u.equipment.length > 0 ? (
+                <Text
+                  style={{
+                    marginRight: 400,
+                    borderBottomColor: "black",
+                    borderBottomWidth: 1,
+                    marginBorder: 2,
+                  }}
+                ></Text>
+              ) : null}
+              {u.equipment.length > 0
+                ? u.equipment.map((e) => (
+                    <View style={{ marginLeft: 35 }}>
+                      <Text style={{ textAlign: "left", fontFamily: "notMaryKate", fontSize: 10 }}>
+                        {/* EQUIPMENT */}
+                        {e.name} - {e.points}
+                      </Text>
+                    </View>
+                  ))
+                : null}
+            </Text>
+          ))}
+        </View>
+      ));
   };
 
   return (
--- src/components/PDFGenerator/PDFBox.js
+++ src/components/PDFGenerator/PDFBox.js
@@ -1,9 +1,10 @@
 // Reac
-import React from "react";
+import React, { useEffect, useState } from "react";
 // react-pdf
 import { PDFViewer } from "@react-pdf/renderer";
 //Material UI
 import { makeStyles } from "@material-ui/core/styles";
+import { CircularProgress } from "@material-ui/core";
 // components and functions
 import ListPDF from "./ListPDF";
 
@@ -18,14 +19,33 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 // Create Document Component
+
 const PdfBox = (props) => {
   const classes = useStyles();
 
-  return (
+  const [pdfMasterList, setPdfMasterList] = useState([]);
+
+  let units = props.units;
+  let distinctSubFactions = props.distinctSubFactions;
+
+  useEffect(() => {
+    let tempArray = [];
+
+    distinctSubFactions.forEach((sF) => {
+      tempArray.push({ subFaction: sF, units: filterForSubFaction(sF) });
+      setPdfMasterList([...tempArray]);
+    });
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  const filterForSubFaction = (subFaction) => {
+    return units.filter((u) => u.subFaction === subFaction);
+  };
+
+  return pdfMasterList.length > 0 ? (
     <PDFViewer className={classes.pdfModal}>
-      <ListPDF units={props.units} distinctSubFactions={props.distinctSubFactions} />
+      <ListPDF pdfMasterList={pdfMasterList} units={props.units} distinctSubFactions={props.distinctSubFactions} />
     </PDFViewer>
-  );
+  ) : null;
 };
 
 export default PdfBox;
