a5bf43998fe60a33decde8a4c4c78c0b8118a190
marcelbade <62931394+marcelbade@users.noreply.github.com>
2023-06-04 13:15:47 +0200

    refactored all pdf styles into a style object

---
+++ src/components/PDFGenerator/pdfStyles.js
@@ -0,0 +1,30 @@
+const styles = {
+  subFactionNameStyle: {
+    textAlign: "left", //
+    fontFamily: "notMaryKate",
+    fontSize: 15,
+  },
+  unitEntryStyle: {
+    textAlign: "left", //
+    fontFamily: "Beryliumbold",
+    fontSize: 10,
+  },
+  equipmentLineStyle: {
+    marginRight: 400,
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
+    marginBorder: 2,
+  },
+  equipmentEntryStyle: {
+    textAlign: "left", //
+    fontFamily: "Beryliumbold",
+    fontSize: 10,
+  },
+  globalMargin: {
+    marginLeft: 15, //
+    marginTop: 8,
+  },
+  equipmentListMargin: { marginLeft: 35 },
+};
+
+export default styles;
