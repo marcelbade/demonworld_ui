8d4673bcad0dbefb0c86c31f028616dbdd64c34a
marcelbade <62931394+marcelbade@users.noreply.github.com>
2023-06-04 13:34:34 +0200

    refactored landing page

---
+++ src/components/landingPage/LandingPageNaviButton.js
@@ -0,0 +1,66 @@
+// React
+import React from "react";
+import { useHistory } from "react-router-dom";
+// Material UI
+import { IconButton, Grid, Tooltip, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  iconContainer: {
+    width: "20em",
+    height: "20em",
+    textAlign: "center",
+
+    [theme.breakpoints.up("md")]: {
+      width: "20em",
+      height: "20em",
+    },
+    [theme.breakpoints.down("md")]: {
+      width: "10em",
+      height: "10em",
+    },
+  },
+  mobileToolTip: {
+    fontSize: "15px",
+    [theme.breakpoints.up("md")]: {
+      display: "none",
+    },
+    [theme.breakpoints.down("md")]: {},
+  },
+
+  tooltipText: {
+    fontSize: "20px",
+  },
+}));
+
+const LandingPageNaviButton = (props) => {
+  const classes = useStyles();
+  const history = useHistory();
+
+  const toPage = (relativeURL) => {
+    history.push({
+      pathname: relativeURL,
+      state: {
+        lastPage: "landingPage",
+        selectedArmy: [],
+      },
+    });
+  };
+
+  return (
+    <Grid item className={classes.iconContainer}>
+      <Tooltip title={<Typography className={classes.tooltipText}>Verlustrechner</Typography>}>
+        <IconButton
+          onClick={() => {
+            toPage(props.relativeURL);
+          }}
+        >
+          <img src={props.icon} alt={props.altText} height={100} width={100} />
+        </IconButton>
+      </Tooltip>
+      <Typography className={classes.mobileToolTip}>Verlustrechner</Typography>
+    </Grid>
+  );
+};
+
+export default LandingPageNaviButton;
--- src/components/landingPage/landingPage.js
+++ src/components/landingPage/landingPage.js
@@ -1,13 +1,15 @@
 // React
 import React from "react";
-import { Link, useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 // Material UI
-import { IconButton, Grid, Tooltip, Typography } from "@material-ui/core";
+import { Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 // icons
 import deathIcon from "../../icons/icons8-death-64.png";
 import calculatorIcon from "../../icons/icons8-calculator-64.png";
 import bookIcon from "../../icons/icons8-book-64.png";
+// functions and components
+import LandingPageNaviButton from "./LandingPageNaviButton";
 
 const useStyles = makeStyles((theme) => ({
   homePage: {
@@ -26,78 +28,16 @@ const useStyles = makeStyles((theme) => ({
       },
     },
   },
-
-  iconContainer: {
-    width: "20em",
-    height: "20em",
-    textAlign: "center",
-
-    [theme.breakpoints.up("md")]: {
-      width: "20em",
-      height: "20em",
-    },
-    [theme.breakpoints.down("md")]: {
-      width: "10em",
-      height: "10em",
-    },
-  },
-  mobileToolTip: {
-    fontSize: "15px",
-    [theme.breakpoints.up("md")]: {
-      display: "none",
-    },
-    [theme.breakpoints.down("md")]: {},
-  },
-
-  tooltipText: {
-    fontSize: "20px",
-  },
 }));
 
 const LandingPage = () => {
   const classes = useStyles();
-  const history = useHistory();
-
-  const toLossCalculator = () => {
-    history.push({
-      pathname: "/lossCalculator",
-      state: {
-        lastPage: "landingPage",
-        selectedArmy: [],
-      },
-    });
-  };
 
   return (
     <Grid container justify="center" alignContent="center" className={classes.homePage}>
-      <Grid item className={classes.iconContainer}>
-        <Tooltip title={<Typography className={classes.tooltipText}>Kompendium</Typography>}>
-          <IconButton component={Link} to="/compendium">
-            <img src={bookIcon} alt="Kompendium" height={100} width={100} />
-          </IconButton>
-        </Tooltip>
-        <Typography className={classes.mobileToolTip}>compendium</Typography>
-      </Grid>
-      <Grid item className={classes.iconContainer}>
-        <Tooltip title={<Typography className={classes.tooltipText}>Armeelistengenerator</Typography>}>
-          <IconButton component={Link} to="/listGenerator">
-            <img src={calculatorIcon} alt="Armeelistengenerator" height={100} width={100} />
-          </IconButton>
-        </Tooltip>
-        <Typography className={classes.mobileToolTip}>Armeelistengenerator</Typography>
-      </Grid>
-      <Grid item className={classes.iconContainer}>
-        <Tooltip title={<Typography className={classes.tooltipText}>Verlustrechner</Typography>}>
-          <IconButton
-            onClick={() => {
-              toLossCalculator();
-            }}
-          >
-            <img src={deathIcon} alt="Verlustrechner" height={100} width={100} />
-          </IconButton>
-        </Tooltip>
-        <Typography className={classes.mobileToolTip}>Verlustrechner</Typography>
-      </Grid>
+      <LandingPageNaviButton relativeURL={"/compendium"} icon={bookIcon} altText={"Kompendium"} />
+      <LandingPageNaviButton relativeURL={"/listGenerator"} icon={calculatorIcon} altText={"ListenGenerator"} />
+      <LandingPageNaviButton relativeURL={"/lossCalculator"} icon={deathIcon} altText={"Verlustrechner"} />
     </Grid>
   );
 };
