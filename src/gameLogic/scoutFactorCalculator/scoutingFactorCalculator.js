b220b2939139617cd06f6c150b63608c5264783b
marcelbade <62931394+marcelbade@users.noreply.github.com>
2023-06-08 10:13:55 +0200

    added file for scouting caculator

---
+++ src/gameLogic/scoutFactorCalculator/scoutingFactorCalculator.js
@@ -0,0 +1,16 @@
+// NACH dem Auslegen der Spielfläche (und even-
+//     tueller Angriffszielmarker), aber VOR der Auf-
+//     stellung der Armeen, geben beide Spieler ihre
+//     Spähfaktoren bekannt. Der Spähfaktor einer
+//     Armee ist gleich der Summe aller ihrer Reiterei-
+//     sowie (im Expertenspiel) Flieger-Elemente, wo-
+//     bei entsprechend ausgerüstete Befehlshaber
+//     mitzählen. Reiterei-Elemente sowie Elemente,
+//     die nur niedrig fliegen können, zählen doppelt,
+//     wenn sie bei einem B-Befehl über mindestens 40
+//     Bewegungspunkte verfügen. Flieger-Elemente
+//     (die auch hoch fliegen können) zählen unabhän-
+//     gig von den ihnen zustehenden Bewegungs-
+//     punkten immer doppelt
+
+// let scoutingFactor = numberOfMountedUnits + numberOflyers + numberOfMountedHeroes + numberOfFlyingHeroes;
