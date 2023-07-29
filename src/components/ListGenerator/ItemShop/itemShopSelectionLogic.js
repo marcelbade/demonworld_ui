c0566fcf394da0f5c765cbf35c50dddfa497bea2
marcelbade <62931394+marcelbade@users.noreply.github.com>
2023-07-29 11:10:36 +0200

    selection logic finished

--- src/components/ListGenerator/ItemShop/itemShopSelectionLogic.js
+++ src/components/ListGenerator/ItemShop/itemShopSelectionLogic.js
@@ -1,47 +1,55 @@
+import { ITEM_TYPE_BANNER, ITEM_TYPE_MUSICIAN } from "../../../constants/itemShopConstants";
 
 /**
- * Functions ckecks if item has already been picked by a different unit.
+ * Functions ckecks if item has already been picked by a different unit. If this is the case AND the item is not generic (i.e., it's unique) it returns true-
  * @param {[String]} allItems names of all items already picked.
  * @param {itemCard Obj} item
  * @returns boolean flag
  */
 export const hasItemBeenPickedByOtherUnit = (allItems, item) => {
-  if (allItems.includes(item.itemName)) {
-    return true;
-  }
-  return false;
+  console.log("allItems");
+  console.log(allItems);
+
+  return allItems.includes(item.itemName);
 };
 
 /**
- * Functions checks if item has already been picked by this unit.
+ * Functions checks if an item has already been picked for this unit. If that's the case, it returns true.
  * @param {unitCard Obj} unit
  * @param {itemCard Obj} item
  * @returns boolean flag
  */
-export const hasItemBeenPicked = (unit, item) => {
-  if (unit.equipment.filter((e) => e.name === item.itemName).length > 0) {
-    //  updateEquipmentFlags(unit, item);
-    return true;
-  }
-  return false;
-};
-
-/**
- * Functions checks if the item type has already been picked (type of non-magical item has already been picked).
- * @param {unitCard Obj} unit
- * @param {itemCard Obj} item
- * @returns boolean flag
- */
-export const hasItemTypeBeenPicked = (unit, item) => {
-  return false;
+export const doesUnitalreadyHaveItem = (unit, item) => {
+  return unit.equipment.filter((e) => e.name === item.itemName).length > 0;
 };
 
 /**
  * Functions checks if the maximum number of magic items has been reached.
  * @param {unitCard Obj} unit
- * @param {itemCard Obj} item
  * @returns boolean flag
  */
 export const ownsMaxNumberMagicItems = (unit, item) => {
-  return false;
+  return unit.equipmentTypes.magicItem && !item.additionalItem;
+};
+
+/**
+ * Functions checks if the maximum number of banner items has been reached.
+ * @param {unitCard Obj} unit
+ * @returns boolean flag
+ */
+export const doesUnitAlreadyHaveBanner = (unit, item) => {
+  if (item.itemType === ITEM_TYPE_BANNER) {
+    return unit.equipmentTypes.banner;
+  } else return false;
+};
+
+/**
+ * Functions checks if the maximum number of banner items has been reached.
+ * @param {unitCard Obj} unit
+ * @returns boolean flag
+ */
+export const doesUnitAlreadyHaveInstrument = (unit, item) => {
+  if (item.itemType === ITEM_TYPE_MUSICIAN) {
+    return unit.equipmentTypes.banner;
+  } else return false;
 };
