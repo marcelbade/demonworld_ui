4e88d2c8a3560b0340f354a1646db4dd43af9c67
marcelbade <62931394+marcelbade@users.noreply.github.com>
2023-08-10 12:26:49 +0200

    added secondSubfaction array to validator

--- src/gameLogic/armyListValidationRules/factionValidationRules/validationResultsObjectProvider.js
+++ src/gameLogic/armyListValidationRules/factionValidationRules/validationResultsObjectProvider.js
@@ -14,6 +14,7 @@ const validationResults = {
   unitsBlockedbyRules: [],
   subFactionBelowMinimum: [],
   removeUnitsNoLongerValid: [],
+  secondSubFactionMissing:[],
   commanderIsPresent: false,
 };
 
