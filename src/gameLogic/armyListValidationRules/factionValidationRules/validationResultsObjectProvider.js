d98d339351d120d556e9ea0bc7d48f555f376415
marcelbade <62931394+marcelbade@users.noreply.github.com>
2023-08-15 17:03:17 +0200

    updated comment

--- src/gameLogic/armyListValidationRules/factionValidationRules/validationResultsObjectProvider.js
+++ src/gameLogic/armyListValidationRules/factionValidationRules/validationResultsObjectProvider.js
@@ -5,9 +5,12 @@
  * - subFactionBelowMinimum: vertain sub faction have minimum points requirement.
  *   If not enough points are spent on units of that sub faction, it is added to the array.
  *
- * - removeUnitsNoLongerValid: some units can only be added to an army list when a condition is met.
+ * - removeUnitsNoLongerValid: some units can only be added to an army list when a condition
+ *   is met.
  *   If the condition is no longer met, they are added to the array to be removed.
  *
+ * - secondSubFactionMissing: if the faction has a rule that some/all units must be assigned
+ *   a secod subFaction, units w/o it are added to thsi array 
  * - commanderIsPresent: Most army construction rules demand the present of a commander.
  */
 const validationResults = {
