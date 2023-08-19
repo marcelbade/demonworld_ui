1aa79e4f432d3263d81f422f89d258d2673651d9
marcelbade <62931394+marcelbade@users.noreply.github.com>
2023-08-19 13:53:07 +0200

    refactored pdf styles

--- src/components/PDFGenerator/pdfStyles/detailedCardPdfStyles.js
+++ src/components/PDFGenerator/pdfStyles/detailedCardPdfStyles.js
@@ -19,28 +19,45 @@ const styles = {
     textAlign: "left",
     marginTop: 5,
     marginLeft: 20,
-    fontSize: 15,
-  },
-
-  CardRow: {
-    margin: "auto",
-    flexDirection: "row",
-    border: "1px black dashed", 
-  },
-
-  CardCol: {
-    width: "30%",
-    fontFamily: "Beryliumbold",
-  },
-
-  CardCell: {
-    fontFamily: "Beryliumbold",
-    margin: "auto",
-    textAlign: "left",
-    marginTop: 5,
-    marginLeft: 20,
     fontSize: 10,
-   
+  },
+
+  //-----------------------------
+
+
+  entireCard: {
+    marginTop: 15,
+    width: "50%",
+    paddingLeft: 25,
+  },
+
+  cardunitName: {
+    fontFamily: "notMaryKate",
+    color: "red",
+    fontSize: 10,
+  },
+
+  cardRow: {
+    width: "100%",
+    fontFamily: "Beryliumbold",
+    fontSize: 10,
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    backgroundColor: "lightgrey",
+  },
+
+  cardBlackRows:{
+    width: "100%",
+    fontFamily: "Beryliumbold",
+    fontSize: 10,
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    color: "white",
+    backgroundColor: "black",
+  },
+
+  rowBorders: {
+    border: "1px black solid",
   },
 };
 
