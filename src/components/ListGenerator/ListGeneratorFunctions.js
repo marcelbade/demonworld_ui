9b766a9783ef621dadb1ad81d0a0d1b371b2b770
marcelbade <62931394+marcelbade@users.noreply.github.com>
2023-08-26 20:26:54 +0200

    added function to listGeneratorFunctions

--- src/components/ListGenerator/ListGeneratorFunctions.js
+++ src/components/ListGenerator/ListGeneratorFunctions.js
@@ -1,5 +1,16 @@
 import { uuidGenerator } from "../shared/sharedFunctions";
 
+
+
+/**
+ * Function filters unit card arrays for a sub faction.
+ * @param {String} subFaction
+ * @returns An array of unit card objects filtered for a a sub faction.
+ */
+export const filterForSubFaction = (units, subFaction) => {
+  return units.filter((u) => u.subFaction === subFaction);
+};
+
 /**
  * Function returns all distinct subFactions of a selected faction.
  * @param {[unitCard object]} units
@@ -64,9 +75,9 @@ const addEquipmentSlotsToUnit = (unit) => {
     ...unit,
     equipment: [],
     equipmentTypes: {
-      magicItem: false, 
+      magicItem: false,
       banner: false,
-      musician:false,
+      musician: false,
     },
   };
 };
