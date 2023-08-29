943d0c4c3c958ac5e4cea53f87fad18773a43031
marcelbade <62931394+marcelbade@users.noreply.github.com>
2023-08-29 23:26:53 +0200

    fixed comment, changed order of functions

--- src/components/ListGenerator/GeneratorComponents/ArmyValidation.js
+++ src/components/ListGenerator/GeneratorComponents/ArmyValidation.js
@@ -9,7 +9,7 @@ const ArmyValidation = () => {
   const AC = useContext(ArmyContext);
 
   /**
-   * Validate the current army list everytime a unit is added, removed or the max point allowance changes.
+   * Function validates the current army list everytime a unit is added, removed, subFactions, the selected alternative army list or the max point allowance changes.
    * Validation works through a validator object. The object returns an array containing all units
    * which need to be blocked, as well as a message stating the reason for blocking it.
    */
@@ -20,23 +20,18 @@ const ArmyValidation = () => {
         AC.listOfAllFactionUnits,
         AC.selectedUnits,
         AC.maxPointsAllowance,
-        AC.subFactions
+        AC.subFactions,
+        AC.selectedAlternativeList
       );
 
       collectValidatioResults(validationResult);
     }
-  }, [AC.selectedUnits, AC.maxPointsAllowance, AC.selectedAlternativeList, AC.distinctSubFactions]); // eslint-disable-line react-hooks/exhaustive-deps
-
-  // Automatically remove units from the army list if the list no longer meets the ciriteria that have to be met to add those units.
-  useEffect(() => {
-    if (AC.listValidationResults.removeUnitsNoLongerValid.length > 0) {
-      let currentState = [...AC.selectedUnits];
-
-      currentState = currentState.filter((u) => !AC.listValidationResults.removeUnitsNoLongerValid.includes(u));
-
-      AC.setSelectedUnits([...currentState]);
-    }
-  }, [AC.listValidationResults]); // eslint-disable-line react-hooks/exhaustive-deps
+  }, [
+    AC.selectedUnits, //
+    AC.maxPointsAllowance,
+    AC.selectedAlternativeList,
+    AC.subFactions,
+  ]); // eslint-disable-line react-hooks/exhaustive-deps
 
   /**
    * Function adds all invalid units to the block list.
@@ -52,6 +47,17 @@ const ArmyValidation = () => {
       secondSubFactionMissing: validationResults.secondSubFactionMissing,
     });
   };
+
+  // Automatically remove units from the army list if the list no longer meets the ciriteria that have to be met to permit those units to be picked.
+  useEffect(() => {
+    if (AC.listValidationResults.removeUnitsNoLongerValid.length > 0) {
+      let currentState = [...AC.selectedUnits];
+
+      currentState = currentState.filter((u) => !AC.listValidationResults.removeUnitsNoLongerValid.includes(u));
+
+      AC.setSelectedUnits([...currentState]);
+    }
+  }, [AC.listValidationResults]); // eslint-disable-line react-hooks/exhaustive-deps
 
   // this component returns no jsx - it is simply meant to help code readability by factoring out all logic for army validation from the ListGeneratorController component.
   return null;
