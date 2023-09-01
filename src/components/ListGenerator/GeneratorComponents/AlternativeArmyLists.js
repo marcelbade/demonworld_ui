290d2de2b5811abbb090dd32736d6502760166d2
marcelbade <62931394+marcelbade@users.noreply.github.com>
2023-09-01 22:47:54 +0200

    revised alternative army list option

--- src/components/ListGenerator/GeneratorComponents/AlternativeArmyLists.js
+++ src/components/ListGenerator/GeneratorComponents/AlternativeArmyLists.js
@@ -26,46 +26,31 @@ const AlternativeArmyLists = () => {
   useEffect(() => {
     if (AC.armyHasAlternativeLists && AC.selectedAlternativeList !== NONE) {
       alternateUnitListFilter();
-      alternateSubFactionFilter();
+      // alternateSubFactionFilter();
     }
   }, [AC.armyHasAlternativeLists, AC.selectedAlternativeList]); // eslint-disable-line react-hooks/exhaustive-deps
 
   /**
-   * Function filters down an army's sub factions to the ones listed in the selected alternative army list.
-   * Alternative army lists work in one of two ways:
-   * either by excluding certain sub factions from the list of sub factions available to the user
-   * or by removing units from the list of all units available.
-   * This function takes an array of all possible choices (all sub factions affected by the alternative army lists),
-   * removes the one option selected by the user and
-   * then uses the resulting array to filter out all units that belong the sub factions excluded by that alternative list.
-   * There is one faction (dwarfs) that requires 2 choices, the second being hard coded.
-   * @param {String} subFaction
-   *
-   * @returns true, if no alternative lists exist or if the subfaction has been selected by the user.
+   * Function filters units, removes all units that do not belong to the selected army list.
    */
   const alternateUnitListFilter = () => {
     const alternatives = ARMY_ALTERNATIVES_LIST_MAPPER[AC.selectedFactionName];
     const notSelected = alternatives.filter((a) => a !== AC.selectedAlternativeList);
 
-    let tempArray = [...AC.listOfAllFactionUnits];
+    let tempArray = [...AC.subFactions];
 
-    tempArray = tempArray.filter((u) => {
-      if (notSelected.includes(u.subFaction)) {
-        return false;
-      }
-      return true;
-    });
+    tempArray = tempArray.filter((f) => !notSelected.includes(f));
 
-    AC.setAlternativeUnitList([...tempArray]);
+    AC.setDistinctSubFactions([...tempArray]);
   };
 
-  const alternateSubFactionFilter = () => {
-    const alternatives = ARMY_ALTERNATIVES_LIST_MAPPER[AC.selectedFactionName];
-    const notSelected = alternatives.filter((a) => a !== AC.selectedAlternativeList);
-    const tempArray = [...AC.subFactions];
+  // const alternateSubFactionFilter = () => {
+  //   const alternatives = ARMY_ALTERNATIVES_LIST_MAPPER[AC.selectedFactionName];
+  //   const notSelected = alternatives.filter((a) => a !== AC.selectedAlternativeList);
+  //   const tempArray = [...AC.subFactions];
 
-    AC.setAlternativeSubFactionList(tempArray.filter((sF) => !notSelected.includes(sF)));
-  };
+  //   AC.setAlternativeSubFactionList(tempArray.filter((sF) => !notSelected.includes(sF)));
+  // };
 
   return (
     <Fragment>
