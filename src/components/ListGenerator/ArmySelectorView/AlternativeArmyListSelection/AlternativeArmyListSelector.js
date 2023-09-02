3ec1ffab5cda25549c2ccc7e0d1fe1898570584e
marcelbade <62931394+marcelbade@users.noreply.github.com>
2023-09-02 17:56:31 +0200

    moved state from this component to central state

--- src/components/ListGenerator/ArmySelectorView/AlternativeArmyListSelection/AlternativeArmyListSelector.js
+++ src/components/ListGenerator/ArmySelectorView/AlternativeArmyListSelection/AlternativeArmyListSelector.js
@@ -1,13 +1,11 @@
 // React
-import React, { useState, useContext, useEffect } from "react";
+import React, { useContext } from "react";
 // Material UI
 import { makeStyles } from "@material-ui/core/styles";
 import { Typography } from "@material-ui/core";
 // components and functions
 import { ArmyContext } from "../../../../contexts/armyContext";
 import SelectionInput from "../../../shared/selectionInput";
-// constants
-import { ARMY_ALTERNATIVES_LIST_MAPPER, ALTERNATIVE_ARMY_SELECTION_TEXT, ZWERGE } from "../../../../constants/factions";
 
 const useStyles = makeStyles((theme) => ({
   alternativeListSelector: {
@@ -22,55 +20,13 @@ const AlternativeArmyListSelector = () => {
   const classes = useStyles();
   const AC = useContext(ArmyContext);
 
-  const [options, setOptions] = useState([]);
-  const [labelText, setLabelText] = useState("");
-
-  useEffect(() => {
-    setOptions(findMenuOptions());
-  }, [AC.selectedFactionName]); // eslint-disable-line react-hooks/exhaustive-deps
-
-  useEffect(() => {
-    setLabelText(findLabelTexts());
-  }, [AC.selectedFactionName]); // eslint-disable-line react-hooks/exhaustive-deps
-
-  /**
-   *Function returns the names of the alternative army lists as options for the drop downm menu.
-   * @returns anr array of string values.
-   */
-  const findMenuOptions = () => {
-    if (AC.armyHasAlternativeLists) {
-      if (AC.selectedFactionName === ZWERGE) {
-        const result = [...ARMY_ALTERNATIVES_LIST_MAPPER[AC.selectedFactionName]];
-
-        result.pop(); 
-        return result;
-      }
-      return ARMY_ALTERNATIVES_LIST_MAPPER[AC.selectedFactionName];
-    }
-  };
-
-  /**
-   * Function retrieves the correct label text for the input element.
-   * @returns String with the label text.
-   */
-  const findLabelTexts = () => {
-    if (AC.armyHasAlternativeLists) {
-      const result =
-        AC.selectedFactionName === ZWERGE
-          ? ALTERNATIVE_ARMY_SELECTION_TEXT[AC.selectedFactionName][0]
-          : ALTERNATIVE_ARMY_SELECTION_TEXT[AC.selectedFactionName];
-
-      return result;
-    }
-  };
-
   return (
     <SelectionInput
       className={classes.alternativeListSelector}
       filterFunction={AC.setSelectedAlternativeList}
       isArmySelector={true}
-      options={options}
-      label={<Typography>{labelText}</Typography>}
+      options={AC.alternateArmyListOptions}
+      label={<Typography>{AC.alternateArmyListLabelText}</Typography>}
     />
   );
 };
