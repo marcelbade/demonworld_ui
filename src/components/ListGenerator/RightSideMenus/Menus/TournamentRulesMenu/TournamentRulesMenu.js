1c1be191c728285d6a521abf7e0323d1885c3a48
marcelbade <62931394+marcelbade@users.noreply.github.com>
2023-09-10 17:55:45 +0200

    finished tournamentRules menu, first draft

--- src/components/ListGenerator/RightSideMenus/Menus/TournamentRulesMenu/TournamentRulesMenu.js
+++ src/components/ListGenerator/RightSideMenus/Menus/TournamentRulesMenu/TournamentRulesMenu.js
@@ -5,11 +5,11 @@ import { Grid, FormControl, FormControlLabel, FormLabel, RadioGroup, Radio, Text
 import { makeStyles } from "@material-ui/core/styles";
 // components and functions
 // Icons
-
+import CloseIcon from "@material-ui/icons/Close";
 // context
 import { ArmyContext } from "../../../../../contexts/armyContext";
 // constants
-import { GENERAL_ERRRORS } from "../../../../../constants/textsAndMessages";
+import { GENERAL_ERRRORS, VALIDATION } from "../../../../../constants/textsAndMessages";
 
 const useStyles = makeStyles({
   overlay: {
@@ -42,10 +42,14 @@ const TournamentRulesMenu = () => {
 
   const [errorMessage, setErrorMessage] = useState("");
   const [buttonsOn, setButtonsOn] = useState("off");
+  const [uniqueRuleOn, setUniqueRuleOn] = useState("off");
   const [disableInput, setDisableInput] = useState(false);
 
-  const handleChange = (event) => {
+  const toggleTournamentRules = (event) => {
     setButtonsOn(event.target.value);
+  };
+  const toggleUniquesRule = (event) => {
+    setUniqueRuleOn(event.target.value);
   };
 
   useEffect(() => {
@@ -56,13 +60,28 @@ const TournamentRulesMenu = () => {
     }
   }, [buttonsOn]); // eslint-disable-line react-hooks/exhaustive-deps
 
+  useEffect(() => {
+    if (uniqueRuleOn === "on") {
+      AC.setTournamentOverrideRules({ ...AC.tournamentOverrideRules, uniquesOnlyOnce: true });
+    } else {
+      AC.setTournamentOverrideRules({ ...AC.tournamentOverrideRules, uniquesOnlyOnce: false });
+    }
+  }, [buttonsOn]); // eslint-disable-line react-hooks/exhaustive-deps
+
   /**
    * Function takes the user input for maximum point allowance, validates it, and sets the state.
    * @param {event object} event
    */
   const changeHeroPercentage = (event) => {
-    AC.setMaxPointsAllowance(event.target.value);
+    AC.setTournamentOverrideRules({ ...AC.tournamentOverrideRules, maxHeroValue: event.target.value });
+    validate();
+  };
+  const changeNonUniqueMax = (event) => {
+    AC.setTournamentOverrideRules({ ...AC.tournamentOverrideRules, nonUniqueMax: event.target.value });
+    validate();
+  };
 
+  const validate = (event) => {
     let isValid = new RegExp(/^[0-9]*$/).test(event.target.value);
     isValid ? setErrorMessage("") : setErrorMessage(GENERAL_ERRRORS.ONLY_NUMBERS);
   };
@@ -72,7 +91,12 @@ const TournamentRulesMenu = () => {
       <Grid item>
         <FormControl>
           <FormLabel id="demo-radio-buttons-group-label">Turnierregeln</FormLabel>
-          <RadioGroup aria-labelledby="demo-radio-buttons-group-label" name="radio-buttons-group" value={buttonsOn} onChange={handleChange}>
+          <RadioGroup
+            aria-labelledby="demo-radio-buttons-group-label"
+            name="radio-buttons-group"
+            value={buttonsOn}
+            onChange={toggleTournamentRules}
+          >
             <FormControlLabel value="on" control={<Radio />} label="An" />
             <FormControlLabel value="off" control={<Radio />} label="Aus" />
           </RadioGroup>
@@ -99,6 +123,42 @@ const TournamentRulesMenu = () => {
           helperText={errorMessage}
           variant="standard"
         />
+      </Grid>
+      <Grid item>
+        <TextField
+          id="outlined-basic"
+          autoComplete="off"
+          value={AC.tournamentOverrideRules.nonUniqueMax}
+          disabled={disableInput}
+          InputProps={{
+            style: {
+              fontSize: "20px",
+              fontWeight: "bold",
+              pading: "50px",
+              width: "110px",
+            },
+            endAdornment: <InputAdornment position="end">Prozent</InputAdornment>,
+          }}
+          onChange={changeNonUniqueMax}
+          required
+          error={Boolean(errorMessage)}
+          helperText={errorMessage}
+          variant="standard"
+        />
+      </Grid>
+      <Grid item>
+        <FormControl>
+          <FormLabel id="demo-radio-buttons-group-label"> {VALIDATION.NO_DUPLICATE_UNIQUES_MESSAGE}</FormLabel>
+          <RadioGroup
+            aria-labelledby="demo-radio-buttons-group-label"
+            name="radio-buttons-group"
+            value={uniqueRuleOn}
+            onChange={toggleUniquesRule}
+          >
+            <FormControlLabel value="on" control={<Radio />} label="An" />
+            <FormControlLabel value="off" control={<Radio />} label="Aus" />
+          </RadioGroup>
+        </FormControl>
       </Grid>
     </Grid>
   );
