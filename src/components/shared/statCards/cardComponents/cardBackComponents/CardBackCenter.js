8bddbac904a966ff2dff1529af4520ffd156fc27
marcelbade <62931394+marcelbade@users.noreply.github.com>
2023-09-20 13:26:21 +0200

    refactored logic for showing equipment on card

--- src/components/shared/statCards/cardComponents/cardBackComponents/CardBackCenter.js
+++ src/components/shared/statCards/cardComponents/cardBackComponents/CardBackCenter.js
@@ -1,15 +1,45 @@
 // React
-import React, { useContext } from "react";
+import React, { useContext, Fragment } from "react";
 // Material UI
-import { Grid } from "@material-ui/core";
+import { Grid, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 // functions and modules
-import { DisplayAllSpecialRules } from "../../../../compendiums/factionTable/depencies/factionTableFunctions";
 import { StateCardContext } from "../../../../../contexts/statCardContext";
 
+const useStyles = makeStyles({
+  specialRules: {
+    fontFamily: "Beryliumbold",
+    paddingLeft: "0.5em",
+  },
+});
+
 const CardBackCenter = () => {
+  const classes = useStyles();
   const SC = useContext(StateCardContext);
 
-  return <Grid item>{DisplayAllSpecialRules(SC.unit)}</Grid>;
+  return (
+    <Grid item>
+      <Typography variant="body1" align="center" className={classes.specialRules}>
+        {SC.unit.specialRules === "" ? "Keine Besonderen Spielregeln" : SC.unit.specialRules}
+        {"equipment" in SC.unit && SC.unit.equipment.length !== 0
+          ? SC.unit.equipment.map((e) => {
+              return (
+                <Fragment>
+                  <hr></hr>
+                  <Typography variant="body1" className={classes.specialRules}>
+                    {e.name}
+                  </Typography>
+                  <Typography>_______</Typography>
+                  <Typography variant="body1" className={classes.specialRules}>
+                    {e.rule}
+                  </Typography>
+                </Fragment>
+              );
+            })
+          : null}
+      </Typography>
+    </Grid>
+  );
 };
 
 export default CardBackCenter;
