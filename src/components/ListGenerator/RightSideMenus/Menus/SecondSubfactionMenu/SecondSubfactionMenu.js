3e3388f1afff1ce9e70066bf0fdae865c4f3c067
marcelbade <62931394+marcelbade@users.noreply.github.com>
2023-09-30 14:51:58 +0200

    finished secondSubFactionMenu, added missing button

--- src/components/ListGenerator/RightSideMenus/Menus/SecondSubfactionMenu/SecondSubfactionMenu.js
+++ src/components/ListGenerator/RightSideMenus/Menus/SecondSubfactionMenu/SecondSubfactionMenu.js
@@ -1,8 +1,10 @@
 // React
 import React, { useContext } from "react";
 //Material UI
-import { Button, Grid, ButtonGroup, Typography } from "@material-ui/core";
+import { Button, Grid, ButtonGroup, Typography, IconButton } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+// icons
+import CancelIcon from "@material-ui/icons/Cancel";
 // components and functions
 import { ArmyContext } from "../../../../../contexts/armyContext";
 import { uuidGenerator } from "../../../../shared/sharedFunctions";
@@ -11,8 +13,9 @@ import { uuidGenerator } from "../../../../shared/sharedFunctions";
 const useStyles = makeStyles({
   overlay: {
     height: "100vh",
-    width: "20vw",
+    width: "30vw",
   },
+  panelButtonBox: {},
   buttons: {
     fontWeight: "bold",
     border: "none",
@@ -26,6 +29,7 @@ const useStyles = makeStyles({
   unitName: {
     fontWeight: "bold",
     borderBottom: "solid 4px black",
+    marginBottom: "1em",
   },
 });
 
@@ -51,7 +55,16 @@ const SecondSubFactionMenu = () => {
 
   return (
     <Grid container direction="column" className={classes.overlay}>
-      <Grid item container direction="row">
+      <Grid item>
+        <IconButton
+          onClick={() => {
+            AC.closeSecondSubFactionMenu();
+          }}
+        >
+          <CancelIcon />
+        </IconButton>
+      </Grid>
+      <Grid item container direction="row" justify="center">
         {/*UNIT NAME */}
         <Grid item xs={9}>
           <Typography variant="h5" align="center" className={classes.unitName}>
@@ -59,39 +72,36 @@ const SecondSubFactionMenu = () => {
           </Typography>
         </Grid>
       </Grid>
-      <Grid item container direction="row">
-        <Grid item xs={3} className={classes.panelButtonsBackground}>
-          {/* PANEL BUTTONS */}
-          <ButtonGroup size="large" orientation="vertical">
-            {AC.secondSubFactionList.map((ssf) => {
-              return ssf === AC.unitSelectedForShop.secondSubFaction ? (
-                <Button
-                  disabled={true}
-                  className={classes.currentlySelected}
-                  variant="text"
-                  key={uuidGenerator()}
-                  onClick={() => {
-                    setSecondSubFactionInArmyList(AC.unitSelectedForShop, ssf);
-                  }}
-                >
-                  {ssf}
-                </Button>
-              ) : (
-                <Button
-                  className={classes.buttons}
-                  variant="text"
-                  key={uuidGenerator()}
-                  onClick={() => {
-                    setSecondSubFactionInArmyList(AC.unitSelectedForShop, ssf);
-                  }}
-                >
-                  {ssf}
-                </Button>
-              );
-            })}
-          </ButtonGroup>
-        </Grid>
-        <Grid item xs={8}></Grid>
+      <Grid item container direction="row" justify="center">
+        {/* PANEL BUTTONS */}
+        <ButtonGroup size="large" orientation="vertical">
+          {AC.secondSubFactionList.map((ssf) => {
+            return ssf === AC.unitSelectedForShop.secondSubFaction ? (
+              <Button
+                disabled={true}
+                className={classes.currentlySelected}
+                variant="text"
+                key={uuidGenerator()}
+                onClick={() => {
+                  setSecondSubFactionInArmyList(AC.unitSelectedForShop, ssf);
+                }}
+              >
+                {ssf}
+              </Button>
+            ) : (
+              <Button
+                className={classes.buttons}
+                variant="text"
+                key={uuidGenerator()}
+                onClick={() => {
+                  setSecondSubFactionInArmyList(AC.unitSelectedForShop, ssf);
+                }}
+              >
+                {ssf}
+              </Button>
+            );
+          })}
+        </ButtonGroup>
       </Grid>
     </Grid>
   );
