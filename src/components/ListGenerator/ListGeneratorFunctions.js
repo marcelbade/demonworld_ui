a825a3ab758ba5a7fb860f72d7d0809d2c2d4a05
marcelbade <62931394+marcelbade@users.noreply.github.com>
2023-10-02 11:50:07 +0200

    moved functions to correct file

--- src/components/ListGenerator/ListGeneratorFunctions.js
+++ src/components/ListGenerator/ListGeneratorFunctions.js
@@ -1,4 +1,3 @@
-import { uuidGenerator } from "../shared/sharedFunctions";
 
 /**
  * Function filters unit card arrays for a sub faction.
@@ -26,56 +25,4 @@ export const findDistinctSubfactions = (units) => {
   return distinctSubFactions;
 };
 
-export const enrichUnitCardObject = (unit) => {
-  unit = addUniqueIdToUnit(unit);
-  unit = addLossCounterToUnit(unit);
-  unit = addEquipmentSlotsToUnit(unit);
-
-  return unit;
-};
-
-/**
- * Functions adds a UUID as unique id so the user can select the
- * same unit twice in a row. Without it, the useEffect does not fire, since the
- * unit objects are identical!
- *
- * @param {} unit
- * @returns {} unit object with a random ID
- */
-const addUniqueIdToUnit = (unit) => {
-  const randomID = uuidGenerator();
-  return { ...unit, uniqueID: randomID };
-};
-
-/**
- * Function adds a property that counts the number of lost elements the unit has suffered. Needed for the loss calculator module.
- * @param {*} unit
- * @returns {} unit object with a lossCounter property
- */
-const addLossCounterToUnit = (unit) => {
-  const max = unit.numberOfElements * unit.hitpoints;
-
-  return {
-    ...unit,
-    lossCounter: 0,
-    maxCounter: max,
-    unitDestroyed: false,
-  };
-};
-
-/**
- * Function adds a property which allows equipment to be added as well as check what equipment can be added.
- * @param {*} unit
- * @returns unit object with equipment + equipmentTypes property.
- */
-const addEquipmentSlotsToUnit = (unit) => {
-  return {
-    ...unit,
-    equipment: [],
-    equipmentTypes: {
-      magicItem: false,
-      banner: false,
-      musician: false,
-    },
-  };
-};
+ 
