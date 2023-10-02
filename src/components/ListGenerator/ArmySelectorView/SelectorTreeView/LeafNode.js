edc11dabe6dd34ff2352c07fdfb487146277ce1a
marcelbade <62931394+marcelbade@users.noreply.github.com>
2023-10-02 11:50:51 +0200

    added comment

--- src/components/ListGenerator/ArmySelectorView/SelectorTreeView/LeafNode.js
+++ src/components/ListGenerator/ArmySelectorView/SelectorTreeView/LeafNode.js
@@ -69,7 +69,7 @@ const LeafNode = (props) => {
 
     return {
       ...unit,
-      lossCounter: 0,
+      lossCounter: 0, 
       maxCounter: max,
       unitDestroyed: false,
     };
@@ -86,6 +86,7 @@ const LeafNode = (props) => {
       equipment: [],
       equipmentTypes: {
         magicItem: false,
+        // units only
         banner: false,
         musician: false,
         unit: false,
