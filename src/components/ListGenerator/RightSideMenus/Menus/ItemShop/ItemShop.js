dd21c8ad9d605faf2ec1e04f65d3d0f68303f1d5
marcelbade <62931394+marcelbade@users.noreply.github.com>
2023-12-23 13:14:20 +0100

    factored out header and tree

--- src/components/ListGenerator/RightSideMenus/Menus/ItemShop/ItemShop.js
+++ src/components/ListGenerator/RightSideMenus/Menus/ItemShop/ItemShop.js
@@ -1,24 +1,11 @@
 // React
-import React, { useState, useContext, useEffect } from "react";
+import React from "react";
 //Material UI
-import { Grid, Typography, IconButton } from "@mui/material";
+import { Grid } from "@mui/material";
 import makeStyles from "@mui/styles/makeStyles";
-import { TreeItem, TreeView } from "@material-ui/lab";
-// icons
-import CancelIcon from "@mui/icons-material/Cancel";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 // components and functions
-import { ItemContext } from "../../../../../contexts/itemContext";
-import { RightMenuContext } from "../../../../../contexts/rightMenuContext";
-import useItemFilters from "../../../../../customHooks/UseItemFilters";
-import { isObjectEmtpy } from "../../../../../util/utilityFunctions";
-import TreeItemNode from "./TreeItemNode";
-import useTreeViewController from "../../../../../customHooks/UseTreeViewController";
-// constants
-import { ITEM_CATEGORY_NAME_MAPPING } from "../../../../../constants/itemShopConstants";
-
-
+import ItemShopHeader from "./ItemShopHeader";
+import ItemShopTree from "./ItemShopTree";
 
 const useStyles = makeStyles({
   overlay: {
@@ -28,88 +15,21 @@ const useStyles = makeStyles({
   treePadding: {
     paddingLeft: "2em",
   },
-
-  unitName: {
-    width: "60%",
-    fontWeight: "bold",
-    borderBottom: "solid 4px black",
-    marginBottom: "1em",
-    fontFamily: "jaapokkiRegular",
-  },
-
-  // TODO rule text for items
-  itemTypeName: {
-    "& .MuiTreeItem-label": {
-      fontFamily: "jaapokkiRegular",
-    },
-  },
 });
 
 const ItemShop = () => {
   const classes = useStyles();
 
-  const IC = useContext(ItemContext);
-  const RC = useContext(RightMenuContext);
-  const filter = useItemFilters();
-  const controller = useTreeViewController();
-
-  const [filteredItemGroups, setFilteredItemGroups] = useState([]);
- 
-  useEffect(() => {
-    if (!isObjectEmtpy(IC.unitSelectedForShop)) {
-      setFilteredItemGroups(filter.filterItemTypesForUnit(IC.unitSelectedForShop, IC.fetchedItems.factionItems));
-    }
-  }, [IC.unitSelectedForShop]); // eslint-disable-line react-hooks/exhaustive-deps
-
-  const closeShopPanel = () => {
-    RC.setItemShopState({ ...RC.itemShopState, show: false });
-  };
-
   return (
     <Grid container direction="column" className={classes.overlay}>
-      <Grid item>
-        <IconButton
-          onClick={() => {
-            closeShopPanel();
-          }}
-          size="large"
-        >
-          <CancelIcon />
-        </IconButton>
-      </Grid>
-      {/* display selected unit's name  */}
-      <Grid item container direction="row" justifyContent="center">
-        <Typography variant="h5" align="center" className={classes.unitName}>
-          {IC.unitSelectedForShop.unitName}
-        </Typography>
-      </Grid>
-      <Grid item container direction="row" justifyContent="flex-start" className={classes.treePadding}>
-        <TreeView
-          aria-label="file system navigator" //
-          defaultCollapseIcon={<ExpandMoreIcon />}
-          defaultExpandIcon={<ChevronRightIcon />}
-          expanded={controller.expansionValue}
-        >
-          {filteredItemGroups.map((dto, i) => {
-            return (
-              <TreeItem
-                className={classes.itemTypeName}
-                nodeId={`${i}`} //
-                onClick={() => controller.getNodeId([`${i}`])}
-                label={ITEM_CATEGORY_NAME_MAPPING[dto.typeName]}
-                key={dto.typeName}
-              >
-                {dto.items.map((item) => {
-                  return filter.filterIndividualItems(IC.unitSelectedForShop, item) ? (
-                    <Grid container>
-                      <TreeItemNode item={item} />
-                    </Grid>
-                  ) : null;
-                })}
-              </TreeItem>
-            );
-          })}
-        </TreeView>
+      <ItemShopHeader />
+
+      <Grid
+        item //
+        justifyContent="flex-start"
+        className={classes.treePadding}
+      >
+        <ItemShopTree />
       </Grid>
     </Grid>
   );
