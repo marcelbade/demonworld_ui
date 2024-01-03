df783fbe12f5e368f6cc93f445931bb125083541
marcelbade <62931394+marcelbade@users.noreply.github.com>
2024-01-03 21:40:05 +0100

    moved css to theme

--- src/components/lossCalculator/LossCalcEquipmentList/EquipmentList.js
+++ src/components/lossCalculator/LossCalcEquipmentList/EquipmentList.js
@@ -2,19 +2,10 @@
 import React, { Fragment } from "react";
 //Material UI
 import { Divider, List } from "@mui/material";
-import makeStyles from "@mui/styles/makeStyles";
 // components and functions
 import EquipmentListEntry from "./EquipmentListEntry";
 
-const useStyles = makeStyles((theme) => ({
-  line: {
-    color: "black",
-  },
-}));
-
 const EquipmentList = (props) => {
-  const classes = useStyles();
-
   /**
    * Function checks, if the unit's equipment list should be displayed.
    * @returns true, if equipment array exists and contains elements.
@@ -30,7 +21,7 @@ const EquipmentList = (props) => {
     <List>
       {doesUnithaveEquipment() ? (
         <Fragment>
-          <Divider variant="middle"   />
+          <Divider variant="middle" />
           {props.unit.equipment.map((e, i) => {
             return (
               <EquipmentListEntry
