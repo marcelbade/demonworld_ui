6eabc6ebf6cc46379591a006cd99f38a6729ad58
marcelbade <62931394+marcelbade@users.noreply.github.com>
2024-01-03 12:30:29 +0100

    added correct button variant and spacing

--- src/components/ListGenerator/RightSideMenus/Menus/OptionButtons/ChoosePdfType.js
+++ src/components/ListGenerator/RightSideMenus/Menus/OptionButtons/ChoosePdfType.js
@@ -4,23 +4,29 @@ import { OPTIONS } from "../../../../../constants/textsAndMessages";
 
 const ChoosePdfType = (props) => {
   return props.display ? (
-    <Grid container direction="row">
-      <Button
-        onClick={() => {
-          props.openPDfInNewTab({ printDefaultList: true });
-          props.setShowPdfVariantButtons(false);
-        }}
-      >
-        {OPTIONS.CREATE_DEFAULT_LIST}
-      </Button>
-      <Button
-        onClick={() => {
-          props.openPDfInNewTab({ printDefaultList: true });
-          props.setShowPdfVariantButtons(false);
-        }}
-      >
-        {OPTIONS.CREATE_DETAILED_LIST}
-      </Button>
+    <Grid container direction="row" justifyContent="space-between">
+      <Grid item xs={5}>
+        <Button
+          variant="outlined"
+          onClick={() => {
+            props.openPDfInNewTab({ printDefaultList: true });
+            props.setShowPdfVariantButtons(false);
+          }}
+        >
+          {OPTIONS.CREATE_DEFAULT_LIST}
+        </Button>
+      </Grid>
+      <Grid item xs={5}>
+        <Button
+          variant="outlined"
+          onClick={() => {
+            props.openPDfInNewTab({ printDefaultList: true });
+            props.setShowPdfVariantButtons(false);
+          }}
+        >
+          {OPTIONS.CREATE_DETAILED_LIST}
+        </Button>
+      </Grid>
     </Grid>
   ) : null;
 };
