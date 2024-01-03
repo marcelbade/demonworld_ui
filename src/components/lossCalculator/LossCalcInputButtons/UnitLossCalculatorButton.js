e6fa90033732e1dc96cb7d96d33be8b4e5bac3d1
marcelbade <62931394+marcelbade@users.noreply.github.com>
2024-01-03 19:55:01 +0100

    fixed wron button element, styling

--- src/components/lossCalculator/LossCalcInputButtons/UnitLossCalculatorButton.js
+++ src/components/lossCalculator/LossCalcInputButtons/UnitLossCalculatorButton.js
@@ -1,39 +1,37 @@
 // React
 import React from "react";
 //Material UI
-import { Button, Tooltip, Typography } from "@mui/material";
-import makeStyles from '@mui/styles/makeStyles';
+import { IconButton, Tooltip, Typography } from "@mui/material";
+import makeStyles from "@mui/styles/makeStyles";
 
 const useStyles = makeStyles((theme) => ({
   bttns: {
-    [theme.breakpoints.up("md")]: {
-      flexDirection: "row",
-    },
-    [theme.breakpoints.down('lg')]: {
-      flexDirection: "row",
-
-      "@media (orientation:landscape)": {
-        flexDirection: "row",
-      },
-    },
+    // [theme.breakpoints.up("md")]: {
+    //   flexDirection: "row",
+    // },
+    // [theme.breakpoints.down('lg')]: {
+    //   flexDirection: "row",
+    //   "@media (orientation:landscape)": {
+    //     flexDirection: "row",
+    //   },
+    // },
   },
 }));
 
 const UnitLossCalculatorButton = (props) => {
   const classes = useStyles();
 
-  return props.displayButton ? (
-    <Tooltip title={<Typography className={classes.tooltipText}>{props.tooltipText}</Typography>}>
-      <Button
-        variant="contained"
+  return props.display ? (
+    <Tooltip title={<Typography variant="h6">{props.tooltipText}</Typography>}>
+      <IconButton
         onClick={() => {
           props.action();
         }}
-        disabled={props.disablerExpression}
+        disabled={props.disableBttn}
         className={classes.bttn}
       >
         {props.icon}
-      </Button>
+      </IconButton>
     </Tooltip>
   ) : null;
 };
