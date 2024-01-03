6b76445fc0a10d23aaa3f0979b68ba18b8315e0f
marcelbade <62931394+marcelbade@users.noreply.github.com>
2024-01-03 19:49:13 +0100

    removed unnecessary padding by switching to IconButton

--- src/components/lossCalculator/LossCalcEquipmentList/EquipmentListElementBttn.js
+++ src/components/lossCalculator/LossCalcEquipmentList/EquipmentListElementBttn.js
@@ -1,34 +1,25 @@
 // React
 import React, { useContext } from "react";
 //Material UI
-import { ListItemIcon } from "@mui/material";
+import { IconButton } from "@mui/material";
 // components and functions
 import { LossCalcContext } from "../../../contexts/LossCalculatorContext";
 // icons
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
-import { ListItemButton } from "@mui/material";
 
 const EquipmentListElementBttn = (props) => {
   const calcContext = useContext(LossCalcContext);
 
-  /**
-   * Function displays the correct icon depending on a Boolean flag.
-   * @returns an html icon element from the Materail UI framework.
-   */
-  const iconToDisplay = () => {
-    return props.isItemLost ? <RemoveCircleOutlineIcon /> : <AddCircleOutlineIcon />;
-  };
-
   return (
-    <ListItemButton
-    disabled={props.disableButton}
+    <IconButton
+      disabled={props.disableButton}
       onClick={() => {
         calcContext.setItemIsLostFlag(props.unit, props.itemName, !props.isItemLost);
       }}
     >
-      <ListItemIcon>{iconToDisplay()}</ListItemIcon>
-    </ListItemButton>
+      {props.isItemLost ? <RemoveCircleOutlineIcon /> : <AddCircleOutlineIcon />}
+    </IconButton>
   );
 };
 
