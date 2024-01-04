d8c3937a4e14bcf199d037e5d89060fc93b09b39
marcelbade <62931394+marcelbade@users.noreply.github.com>
2024-01-04 17:38:02 +0100

    removed styling

--- src/components/lossCalculator/LossCalcInputButtons/UnitLossCalculatorButton.js
+++ src/components/lossCalculator/LossCalcInputButtons/UnitLossCalculatorButton.js
@@ -2,25 +2,8 @@
 import React from "react";
 //Material UI
 import { IconButton, Tooltip, Typography } from "@mui/material";
-import makeStyles from "@mui/styles/makeStyles";
-
-const useStyles = makeStyles((theme) => ({
-  bttns: {
-    // [theme.breakpoints.up("md")]: {
-    //   flexDirection: "row",
-    // },
-    // [theme.breakpoints.down('lg')]: {
-    //   flexDirection: "row",
-    //   "@media (orientation:landscape)": {
-    //     flexDirection: "row",
-    //   },
-    // },
-  },
-}));
 
 const UnitLossCalculatorButton = (props) => {
-  const classes = useStyles();
-
   return props.display ? (
     <Tooltip title={<Typography variant="h6">{props.tooltipText}</Typography>}>
       <IconButton
@@ -28,7 +11,6 @@ const UnitLossCalculatorButton = (props) => {
           props.action();
         }}
         disabled={props.disableBttn}
-        className={classes.bttn}
       >
         {props.icon}
       </IconButton>
