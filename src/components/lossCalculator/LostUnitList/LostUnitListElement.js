2fefd724b0300013887a15aa8f579177fdf4c2a9
marcelbade <62931394+marcelbade@users.noreply.github.com>
2024-01-04 17:39:08 +0100

    moved styling to theme, added factored out button

--- src/components/lossCalculator/LostUnitList/LostUnitListElement.js
+++ src/components/lossCalculator/LostUnitList/LostUnitListElement.js
@@ -2,65 +2,27 @@
 import React, { useContext } from "react";
 //Material UI
 import { Typography, Grid } from "@mui/material";
-import makeStyles from '@mui/styles/makeStyles';
+import makeStyles from "@mui/styles/makeStyles";
 // components and functions
 import { LossCalcContext } from "../../../contexts/LossCalculatorContext";
 import { ListItem } from "@mui/material";
 import UnitLossCalcBttnGroup from "../LossCalcInputButtons/UnitLossCalcBttnGroup";
-
-// clsx
-import clsx from "clsx";
 import EquipmentList from "../LossCalcEquipmentList/EquipmentList";
-import { LOSS_CALCULATOR } from "../../../constants/textsAndMessages";
 import ListElementName from "./ListElementName";
-// icons
+import TotalLossButton from "../LossCalcInputButtons/TotalLossButton";
+// constants
+import { LOSS_CALCULATOR } from "../../../constants/textsAndMessages";
 
-const useStyles = makeStyles((theme) => ({
-  listElement: {
-    border: "solid 0.1em",
-    borderRadius: "4px",
-
-    [theme.breakpoints.up("md")]: {
-      flexDirection: "row",
-      padding: "2em",
-      width: "190%",
-      height: "10%",
-    },
-    [theme.breakpoints.down('lg')]: {
-      flexDirection: "column",
-
-      width: "100%",
-      height: "35%",
-      "@media (orientation:landscape)": {
-        flexDirection: "row",
-      },
-    },
-  },
+const useStyles = makeStyles(() => ({
   borderNormal: {
     borderColor: "black",
+    border: "solid 0.1em",
+    borderRadius: "4px",
   },
   borderLost: {
     borderColor: "red",
-  },
-
-  typographyFont: {
-    textAlign: "center",
-    marginTop: "0.5em",
-  },
-  text: {
-    paddingLeft: "1em",
-  },
-  strikeTroughText: {
-    paddingLeft: "1em",
-    color: "red",
-    textDecorationLine: "line-through",
-    textDecorationThickness: "0.2em",
-  },
-  line: {
-    marginTop: "0.5em",
-    marginBottom: "0.5em",
-    borderBottom: "solid black 0.1em",
-    display: "block",
+    border: "solid 0.1em",
+    borderRadius: "4px",
   },
 }));
 
@@ -72,31 +34,46 @@ const LostUnitListElement = (props) => {
     <ListItem>
       <Grid
         container
+        direction="row"
         alignItems="center"
         alignContent="center"
-        className={
-          props.unit.unitDestroyed ? clsx(classes.listElement, classes.borderLost) : clsx(classes.listElement, classes.borderNormal)
-        }
+        justifyContent="center"
+        className={props.unit.unitDestroyed ? classes.borderLost : classes.borderNormal}
       >
-        <Grid container item md={4} direction="column">
-          <ListElementName //
-            unitName={props.unit.unitName}
-            unitDestroyedd={props.unit.unitDestroyed}
-          />
-          <Grid item xs={12}>
+        <Grid container item xs={5} direction="column">
+          <Grid item>
+            <ListElementName //
+              unitName={props.unit.unitName}
+              unitDestroyed={props.unit.unitDestroyed}
+            />
+          </Grid>
+          <Grid item>
             <EquipmentList unit={props.unit} />
           </Grid>
         </Grid>
-        <Grid item>
-          <Typography variant="button" className={clsx(classes.typographyFont, classes.text)}>
+        <Grid item xs={1} sx={{ backgroundColor: "green" }}>
+          <Typography variant="button">
             {calcContext.isHeroMageOrGiantElement(props.unit) ? LOSS_CALCULATOR.TEXT_SINGLE_ELEMENTS : LOSS_CALCULATOR.TEXT_UNITS}
           </Typography>
         </Grid>
-        <Grid item>
+        <Grid
+          item
+          container
+          direction="row" //
+          alignItems="center"
+          justifyContent="center"
+          xs={2}
+          sx={{ backgroundColor: "yellow" }}
+        >
           <UnitLossCalcBttnGroup unit={props.unit} />
         </Grid>
-        <Grid item>
-          <Typography variant="h6" align="center" className={classes.typographyFont}>
+        <Grid item xs={1}>
+          <TotalLossButton unit={props.unit} />
+        </Grid>
+
+        <Grid item xs={1}>
+          <Typography variant="h6" align="center">
+            {/* //TODO   */}
             {calcContext.unitPointsLost}
           </Typography>
         </Grid>
