b23d5d30687743c47c9f642d22bf6801e91608e7
marcelbade <62931394+marcelbade@users.noreply.github.com>
2024-01-05 16:21:11 +0100

    moved styling

--- src/components/lossCalculator/LossCalcEquipmentList/EquipmentListItemName.js
+++ src/components/lossCalculator/LossCalcEquipmentList/EquipmentListItemName.js
@@ -2,31 +2,12 @@
 import React from "react";
 //Material UI
 import { Grid, ListItemText } from "@mui/material";
-import makeStyles from "@mui/styles/makeStyles";
 // component and functions
 import ContextHelpButton from "../../shared/ContextHelpButton";
 // constants
 import { LOSS_CALCULATOR, PUSH_MESSAGE_TYPES } from "../../../constants/textsAndMessages";
 
-const useStyles = makeStyles((theme) => ({
-  strikeTroughText: {
-    color: "red",
-    textDecorationLine: "line-through",
-    textDecorationThickness: "0.2em",
-  },
-}));
-
 const EquipmentListItemName = (props) => {
-  const classes = useStyles();
-
-  /**
-   * Function returns the correct css conditionally.x
-   * @returns an object containing css.
-   */
-  const switchCssClass = () => {
-    return props.isItemLost ? classes.strikeTroughText : null;
-  };
-
   return props.notSingleElementItem ? (
     <Grid container direction="row" justifyContent="flex-start">
       <Grid item xs={4}>
