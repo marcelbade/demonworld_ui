a01a5dd7775c40535ee419f147bcf835fca0aa2c
marcelbade <62931394+marcelbade@users.noreply.github.com>
2024-01-05 16:12:58 +0100

    changed state to object

--- src/customHooks/UsePushMessages.js
+++ src/customHooks/UsePushMessages.js
@@ -10,23 +10,33 @@ import { PUSH_MESSAGE_TYPES } from "../constants/textsAndMessages";
 const usePushMessages = () => {
   const { enqueueSnackbar, closeSnackbar } = useSnackbar();
 
-  const [messageVariant, setMessageVariant] = useState(PUSH_MESSAGE_TYPES.ERROR);
+  const [messageVariant, setMessageVariant] = useState({
+    type: PUSH_MESSAGE_TYPES.ERROR, //
+    mustPersist: false,
+  });
 
   const showSnackBar = (text, messageType) => {
     switch (messageType) {
       case PUSH_MESSAGE_TYPES.ERROR:
         break;
       case PUSH_MESSAGE_TYPES.INFO:
-        setMessageVariant(PUSH_MESSAGE_TYPES.INFO);
+        setMessageVariant({
+          ...messageVariant,
+          type: PUSH_MESSAGE_TYPES.INFO, //
+          mustPersist: true,
+        });
         break;
       default:
         throw new Error("Hook received unknown push message type.");
     }
 
+    console.log("messageVariant");
+    console.log(messageVariant);
+
     enqueueSnackbar(text, {
-      persist: false,
+      persist: messageVariant.mustPersist,
       autoHideDuration: 3000,
-      variant: messageVariant,
+      variant: messageVariant.type,
       action: () => (
         <IconButton
           size="small" //
