938cfb0d38aa09b826baf9e173310f22017f9e30
marcelbade <62931394+marcelbade@users.noreply.github.com>
2024-01-05 16:22:00 +0100

    deleted old logic

--- src/components/lossCalculator/LossCalcInputButtons/TotalLossButton.js
+++ src/components/lossCalculator/LossCalcInputButtons/TotalLossButton.js
@@ -1,7 +1,7 @@
 // react
 import React, { useContext } from "react";
 // material ui
-import { Tooltip, IconButton, Typography } from "@material-ui/core";
+import { IconButton } from "@material-ui/core";
 // functions and components
 import CustomIcon from "../../shared/statCards/CustomIcon";
 import { LossCalcContext } from "../../../contexts/LossCalculatorContext";
@@ -9,8 +9,7 @@ import { LossCalcContext } from "../../../contexts/LossCalculatorContext";
 import skullsIcon from "../../../assets/icons/skulls.png";
 // constants
 import { LOSS_CALCULATOR } from "../../../constants/textsAndMessages";
-import { GIANT, HERO, MAGE } from "../../../constants/unitTypes";
-
+ 
 const TotalLossButton = (props) => {
   const calcContext = useContext(LossCalcContext);
 
@@ -32,27 +31,6 @@ const TotalLossButton = (props) => {
    */
   const allItemsMarkedLost = () => {
     props.unit.equipment.forEach((e) => (e.itemLost = true));
-  };
-
-  /**
-   * Functions selects the correct message to be displayed as tool tip if the
-   * unit has been completely destroyed.
-   * @returns the correct message from the text file.
-   */
-  const displayToolTipUnitLost = () => {
-    let message;
-
-    if (props.unit.unitType === HERO) {
-      message = LOSS_CALCULATOR.LOST_HERO;
-    } else if (props.unit.unitType === MAGE) {
-      message = LOSS_CALCULATOR.LOST_MAGE;
-    } else if (props.unit.unitType === GIANT) {
-      message = LOSS_CALCULATOR.LOST_GIANT;
-    } else {
-      message = LOSS_CALCULATOR.UNIT_ROUTED;
-    }
-
-    return message;
   };
 
   return (
