e5d965d402c6327f41fbaec91bf3a41c6585d00e
marcelbade <62931394+marcelbade@users.noreply.github.com>
2024-01-05 16:22:31 +0100

    removed tooptip

--- src/components/lossCalculator/LossCalcInputButtons/UnitLossCalculatorButton.js
+++ src/components/lossCalculator/LossCalcInputButtons/UnitLossCalculatorButton.js
@@ -1,20 +1,18 @@
 // React
 import React from "react";
 //Material UI
-import { IconButton, Tooltip, Typography } from "@mui/material";
+import { IconButton } from "@mui/material";
 
 const UnitLossCalculatorButton = (props) => {
   return props.display ? (
-    <Tooltip title={<Typography variant="h6">{props.tooltipText}</Typography>}>
-      <IconButton
-        onClick={() => {
-          props.action();
-        }}
-        disabled={props.disableBttn}
-      >
-        {props.icon}
-      </IconButton>
-    </Tooltip>
+    <IconButton
+      onClick={() => {
+        props.action();
+      }}
+      disabled={props.disableBttn}
+    >
+      {props.icon}
+    </IconButton>
   ) : null;
 };
 
