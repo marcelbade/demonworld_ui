d3e34792f51a4f5367a5354292c1d799ca42d496
marcelbade <62931394+marcelbade@users.noreply.github.com>
2024-01-06 16:02:25 +0100

    style bug solved

--- src/components/lossCalculator/LossCalcEquipmentList/EquipmentListItemName.js
+++ src/components/lossCalculator/LossCalcEquipmentList/EquipmentListItemName.js
@@ -1,18 +1,56 @@
 // React
 import React from "react";
 //Material UI
-import { Grid, ListItemText } from "@mui/material";
+import { Grid, ListItemText, Typography } from "@mui/material";
+import makeStyles from "@mui/styles/makeStyles";
 // component and functions
 import ContextHelpButton from "../../shared/ContextHelpButton";
 // constants
 import { LOSS_CALCULATOR, PUSH_MESSAGE_TYPES } from "../../../constants/textsAndMessages";
 
+const useStyles = makeStyles((theme) => ({
+  entry: {
+    [theme.breakpoints.up("md")]: {
+      flexDirection: "row",
+    },
+    [theme.breakpoints.down("lg")]: {
+      flexDirection: "row",
+
+      "@media (orientation:landscape)": {
+        flexDirection: "row",
+      },
+    },
+  },
+  strikeTroughText: {
+    textAlign: "left",
+    marginTop: "0.5em",
+    color: "red",
+    textDecorationLine: "line-through",
+    textDecorationThickness: "0.2em",
+  },
+}));
+
 const EquipmentListItemName = (props) => {
+  const classes = useStyles();
+
+  /**
+   * Function returns the correct css conditionally.x
+   * @returns an object containing css.
+   */
+  const switchCssClass = () => {
+    return props.isItemLost ? classes.strikeTroughText : null;
+  };
+
   return props.notSingleElementItem ? (
+    <ListItemText
+      primary={<span>{props.itemName}</span>} //
+      secondary={<span> {props.pointCost}</span>}
+    />
+  ) : (
     <Grid container direction="row" justifyContent="flex-start">
       <Grid item xs={4}>
         <ListItemText
-          primary={<span> {props.itemName}</span>} //
+          primary={<span className={switchCssClass()}>{props.itemName}</span>} //
           secondary={<span> {props.pointCost}</span>}
         />
       </Grid>
@@ -21,11 +59,6 @@ const EquipmentListItemName = (props) => {
         type={PUSH_MESSAGE_TYPES.INFO}
       />
     </Grid>
-  ) : (
-    <ListItemText
-      primary={<span>{props.itemName}</span>} //
-      secondary={<span> {props.pointCost}</span>}
-    />
   );
 };
 
