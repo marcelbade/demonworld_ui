c75ebfdaac08072475b98ac4575b64c24430d18d
marcelbade <62931394+marcelbade@users.noreply.github.com>
2024-01-07 13:22:43 +0100

    added logic for changing weapon stats in real time

--- src/components/ListGenerator/RightSideMenus/Menus/ItemShop/ItemLogic/StatChangesLogic.js
+++ src/components/ListGenerator/RightSideMenus/Menus/ItemShop/ItemLogic/StatChangesLogic.js
@@ -1,30 +1,114 @@
+/**
+ * Function calculates the value of a unit's first weapon
+ * (weapon 1)
+ * @param {unitCard} unit
+ * @returns the value for the property weapon1
+ */
+export const weapon1stats = (unit) => {
+  let weapon1Properties = { name: unit.weapon1Name, value: unit.weapon1 };
+  const result = searchForRelevantModifier(unit, "altersWeapon1");
 
-/*Wenn ein Element mit einer separat eingekauften
-Nahkampfwaffe ausgestattet wird, müssen Sie zum
-Kampffaktor der Waffe die Größe der Modelle
-hinzurechnen. Sollte das Element ein Passagier auf
-einem Streitwagen sein, so rechnen Sie zur Größe des
-Elements noch die Größe der Plattform des
-Streitwagens mit + 1 hinzu. Sollte das Element
-Passagier auf einem großen Monster, wie einem
-Drachen, sein, so rechnen Sie statt der Größe des
-Elements die Größe des großen Monsters bis zu einem
-maximalen Wert von 4 hinzu.
-Des Weiteren müssen Sie einen Bonus für ein eventuell
-vorhandenes, mitkämpfendes Reittier (z. B. Handwaffe
-+ Wolf), einen Bonus für sonstige elementspezifische
-nahkampffaktor-erhöhende Faktoren (z. B. Speer + Huf)
-sowie einen Bonus für das Kämpfen in geschlossener
-Formation (fünf Figuren auf Standardbasis)
-berücksichtigen. Jeder vorhandene Bonus erhöht den
-endgültigen Kampffaktor um '+2'.*/
+  if (result.modifierFound) {
+    weapon1Properties = {
+      ...weapon1Properties, //
+      name: result.newName,
+      value: calculateNewMeleeWeaponValue(unit, result.modifier),
+    };
+  }
 
+  return weapon1Properties;
+};
 
-export const calculateNewMeleeWeaponValue = (meleeWeapon, unit) => {
+/**
+ * Function iterates througha units items, if they exist.
+ * If one of the items has the correct property with a value > 0
+ * it is returned. If several items fullfill the requirement,
+ * the largest value is returnd.
+ * @param {unitCard} unit
+ * @param {String} property
+ * @returns the modifier for the given unit property. If none is found, 0 is returned.
+ */
+const searchForRelevantModifier = (unit, property) => {
+  let replacementStats = {
+    modifierFound: false,
+    name: null,
+    modifier: 0,
+  };
 
-   let damage =  meleeWeapon.weapon1 + unit.unitSize; 
-   
-   
+  if (unit.equipment !== undefined && unit.equipment.length !== 0) {
+    unit.equipment.forEach((e) => {
+      const itemFields = Object.entries(e);
+      itemFields.forEach((i) => {
+        //i = [property, value]
+        if (i[0] === property && i[1] > 0 && i[1] > replacementStats.modifier) {
+          replacementStats = {
+            ...replacementStats, //
+            modifierFound: true,
+            newName: e.itemName,
+            modifier: i[1],
+          };
+        }
+      });
+    });
+  }
 
+  return replacementStats;
+};
 
+const calculateNewMeleeWeaponValue = (unit, modifier) => {
+  const MAX_SIZE = 4;
+  const LEADER_BONUS = 1;
+  const BONUS = 2;
+
+  let newWeapon1Value = modifier;
+
+  // size bonus - capped at 4 for giant mounts
+  if (unit.unitSize <= MAX_SIZE) {
+    newWeapon1Value += unit.unitSize;
+  } else {
+    newWeapon1Value += MAX_SIZE;
+  }
+
+  // leader always has +1
+  if (unit.leader) {
+    newWeapon1Value += LEADER_BONUS;
+  }
+
+  // has mount
+  if (unit.isMounted) {
+    newWeapon1Value += BONUS;
+  }
+
+  // 5 Miniatures per base
+  //  TODO add to DB!
+  if (unit.closedOrder) {
+    newWeapon1Value += BONUS;
+  }
+
+  // second hand weapon
+  if (unit.twoHandWeapons) {
+    newWeapon1Value += BONUS;
+  }
+
+  return newWeapon1Value;
+};
+
+export const meleeArmorValue = (unit, item) => {
+  return item.altersArmourMelee //
+    ? calculateNewMeleeArmorValue(unit, item)
+    : unit.armourMelee;
+};
+
+const calculateNewMeleeArmorValue = (unit, item) => {
+  return unit.armourMelee + item.altersArmourMelee;
+};
+
+export const rangeArmorValue = (unit, item) => {
+  return item.altersArmourRange //
+    ? calculateNewRangeArmorValue(unit, item)
+    : unit.armourRange;
+};
+
+const calculateNewRangeArmorValue = (unit, item) => {
+  return unit.armourRange + item.altersArmourRange;
 };
--- src/components/shared/statCards/cardComponents/cardFrontComponents/CardFrontCenterComponents/MeleeWeapons.js
+++ src/components/shared/statCards/cardComponents/cardFrontComponents/CardFrontCenterComponents/MeleeWeapons.js
@@ -4,21 +4,35 @@ import React, { useContext } from "react";
 import { Grid, Typography } from "@mui/material";
 // components & functions
 import { StateCardContext } from "../../../../../../contexts/statCardContext";
-// constants
+import { weapon1stats } from "../../../../../ListGenerator/RightSideMenus/Menus/ItemShop/ItemLogic/StatChangesLogic";
 
 const MeleeWeapons = () => {
   const SC = useContext(StateCardContext);
 
+  const weapon1Properties = weapon1stats(SC.unit);
+
   return (
     <Grid item container direction="column">
       <Grid item container justifyContent="center">
-        <Typography variant="h6">{SC.unit.weapon1 === 0 ? null : SC.unit.weapon1Name + ": " + SC.unit.weapon1}</Typography>
+        <Typography variant="h6">
+          {SC.unit.weapon1 === 0 //
+            ? null
+            : `${weapon1Properties.name}: ${weapon1Properties.value}`}
+        </Typography>
       </Grid>
       <Grid item container justifyContent="center">
-        <Typography variant="h6">{SC.unit.weapon2 === 0 ? null : SC.unit.weapon2Name + ": " + SC.unit.weapon2}</Typography>
+        <Typography variant="h6">
+          {SC.unit.weapon2 === 0 //
+            ? null
+            : `${SC.unit.weapon2Name}: ${SC.unit.weapon2}`}
+        </Typography>
       </Grid>
       <Grid item container justifyContent="center">
-        <Typography variant="h6">{SC.unit.weapon3 === 0 ? null : SC.unit.weapon3Name + ": " + SC.unit.weapon3}</Typography>
+        <Typography variant="h6">
+          {SC.unit.weapon3 === 0 //
+            ? null
+            : `${SC.unit.weapon3Name}: ${SC.unit.weapon3}`}
+        </Typography>
       </Grid>
     </Grid>
   );
