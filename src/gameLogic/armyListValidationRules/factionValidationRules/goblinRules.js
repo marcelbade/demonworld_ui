953a9eaf1f90f393f7419f4c49fbb21377d8bac4
marcelbade <62931394+marcelbade@users.noreply.github.com>
2024-02-24 20:45:09 +0100

    fixed goblin rules

--- src/gameLogic/armyListValidationRules/factionValidationRules/goblinRules.js
+++ src/gameLogic/armyListValidationRules/factionValidationRules/goblinRules.js
@@ -1,7 +1,11 @@
 import { GOBLINS } from "../../../constants/textsAndMessages";
+import { HERO, MAGE } from "../../../constants/unitTypes";
 import globalRules from "../globalValidationRules/globalValidationRules";
 import validationResults from "./validationResultsObjectProvider";
 
+/**
+ * A goblin army has only one special rule - the amounts spent on shamans and heroes cannot exceed 40%.
+ */
 const rules = [
   {
     subFaction: "infantry",
@@ -21,7 +25,7 @@ const rules = [
     subFaction: "characters",
     min: 0.0,
     max: 0.3,
-    cardNames: ["Helden/Befehlshaber"],
+    cardNames: ["Helden / Befehlshaber"],
     error: GOBLINS.SUB_FACTION_RULES.CHARACTERS,
   },
   {
@@ -66,6 +70,10 @@ const GoblinRules = {
     tournamentOverrideRules,
     listOfAlliedUnits
   ) => {
+    const MAX_COPIES = 2;
+    // faction rule => 30% cap
+    const HERO_LIMIT = 30;
+
     //  general rules
     let isExceedingPointAllowance = globalRules.armyMustNotExceedMaxAllowance(selectedUnits, availableUnits, totalPointsAllowance);
     let isBelowSubFactionMin = globalRules.unitsBelowSubfactionMinimum(rules, selectedUnits, totalPointsAllowance, subFactions);
@@ -80,27 +88,29 @@ const GoblinRules = {
       maxCopies = tournamentOverrideRules.maxNumber;
       heroPointCap = tournamentOverrideRules.maxHeroValue;
     } else {
-      maxCopies = 2;
-      // faction rule => 40% cap
-      heroPointCap = 40;
+      maxCopies = MAX_COPIES;
+
+      heroPointCap = HERO_LIMIT;
     }
 
     let testForMax2Result = globalRules.maximumCopiesOfUnit(selectedUnits, maxCopies);
-    let testForHeroCapResult = globalRules.belowMaxPercentageHeroes(selectedUnits, totalPointsAllowance, availableUnits, heroPointCap);
+    // let testForHeroCapResult = globalRules.belowMaxPercentageHeroes(selectedUnits, totalPointsAllowance, availableUnits, heroPointCap);
 
     let hasDuplicateUniques = tournamentOverrideRules.uniquesOnlyOnce //
       ? globalRules.noDuplicateUniques(selectedUnits)
       : [];
 
-    // special faction rules - there are no special rules for the Goblins.
+    // special faction rules
+    let testForTotalShamanAndHeroesLimit = totalPointsForShamansAndHeroes(selectedUnits, availableUnits, totalPointsAllowance);
 
     //result for maximum limits
     validationResults.unitsBlockedbyRules = [
       ...isExceedingPointAllowance,
       ...hasDuplicateUniques,
-      ...testForHeroCapResult,
+      // ...testForHeroCapResult,
       ...testForMax2Result,
       ...isAboveSubFactionMax,
+      ...testForTotalShamanAndHeroesLimit,
     ];
     // result for sub factions below limit.
     validationResults.subFactionBelowMinimum = isBelowSubFactionMin;
@@ -110,6 +120,39 @@ const GoblinRules = {
 
     return validationResults;
   },
+};
+
+//FACTION SPECIAL RULES
+
+/**
+ * The army can only consist of 30% shamans and heroes.
+ * @param {[unitcard]} selectedUnits
+ * @param {int} totalPointsAllowance
+ * @returns an array where each element is an object with blocked unit and an error message giving the reaosn
+ * for the block.
+ */
+const totalPointsForShamansAndHeroes = (selectedUnits, availableUnits, totalPointsAllowance) => {
+  const SHAMAN_AND_HEROES_LIMIT = 40;
+  const limit = (totalPointsAllowance * SHAMAN_AND_HEROES_LIMIT) / 100;
+
+  let shamansAndHeroesTotal = 0;
+  let result = [];
+
+  selectedUnits
+    .filter((u) => u.unitType === HERO || u.unitType === MAGE)
+    .forEach((u) => {
+      shamansAndHeroesTotal = shamansAndHeroesTotal + u.points;
+    });
+
+  availableUnits
+    .filter((u) => u.unitType === HERO || u.unitType === MAGE)
+    .forEach((u) => {
+      if (shamansAndHeroesTotal + u.points > limit) {
+        result.push({ unitBlockedbyRules: u.unitName, message: GOBLINS.ERRORS.SHAMAN_AND_HEROES });
+      }
+    });
+
+  return result;
 };
 
 export { GoblinRules, rules };
