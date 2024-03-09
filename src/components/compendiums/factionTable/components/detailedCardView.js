9130b2539ec60e80deb22ed9cf0317cc73b5779c
marcelbade <62931394+marcelbade@users.noreply.github.com>
2024-03-09 12:23:56 +0100

    css refactor

--- src/components/compendiums/factionTable/components/detailedCardView.js
+++ src/components/compendiums/factionTable/components/detailedCardView.js
@@ -1,18 +1,20 @@
 // React
 import React from "react";
-import makeStyles from '@mui/styles/makeStyles';
+import makeStyles from "@mui/styles/makeStyles";
 // components & functions
 import { unitOrCmdCard } from "../../../../util/utilityFunctions";
- 
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   backGround: {
-    backgroundColor: "white",
+    backgroundColor: theme.palette.backGround,
     "& :hover": {
-      backgroundColor: "white",
+      backgroundColor: theme.palette.backGround,
     },
   },
-});
+  card: {
+    paddingBottom: "1em",
+  },
+}));
 
 // front and back side of the displayed unit cards are alligned horizontally.
 const ROW = "row";
@@ -27,8 +29,11 @@ const DetailedCardView = (props) => {
 
   return (
     <tr key={props.unit.uniqueID} className={classes.backGround}>
-      <td key={props.unit.uniqueID} colSpan="100%">
-        {props.selectedCards.includes(props.unit.faction + props.unit.unitName) ? unitOrCmdCard(props.unit, ROW) : null}
+      <td colSpan={"10%"}></td>
+      <td key={props.unit.uniqueID} colSpan={"30%"} className={classes.card}>
+        {props.selectedCards.includes(props.unit.faction + props.unit.unitName) //
+          ? unitOrCmdCard(props.unit, ROW)
+          : null}
       </td>
     </tr>
   );
