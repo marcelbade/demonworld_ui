f4e65e948ce6f78e3b336c9413aed2ae3b311137
marcelbade <62931394+marcelbade@users.noreply.github.com>
2024-03-18 10:27:08 +0100

    added logic to invert custom icon color

--- src/components/shared/statCards/CustomIcon.js
+++ src/components/shared/statCards/CustomIcon.js
@@ -1,12 +1,28 @@
-import React from "react";
+// React
+import React, { useContext } from "react";
+// Material UI
+import makeStyles from "@mui/styles/makeStyles";
+// components and functions
+import { LightSwitchContext } from "../../../contexts/lightSwitchContext";
+
+const useStyles = makeStyles((theme) => ({
+  invertIcon: {
+    webkitFilter: `invert(100%)`,
+    filter: `invert(100%)`,
+  },
+}));
 
 const CustomIcon = (props) => {
+  const classes = useStyles();
+  const LC = useContext(LightSwitchContext);
+
   return (
     <img
       src={props.icon} //
       alt={props.altText}
       height={props.height}
       width={props.width}
+      className={!LC.darkModeOff ? classes.invertIcon : null}
     />
   );
 };
