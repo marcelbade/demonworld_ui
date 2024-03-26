9dbb342d9aa5d8d7820c498412e7ee443701fe90
marcelbade <62931394+marcelbade@users.noreply.github.com>
2024-03-26 19:55:24 +0100

    Replaced German text.

--- src/components/shared/statCards/cardComponents/cardBackComponents/CardBackCenter.js
+++ src/components/shared/statCards/cardComponents/cardBackComponents/CardBackCenter.js
@@ -1,29 +1,34 @@
 // React
 import React, { useContext, Fragment } from "react";
 // Material UI
+import { useTheme } from "@emotion/react";
 import { Grid, Typography } from "@mui/material";
 // functions and modules
 import { StateCardContext } from "../../../../../contexts/statCardContext";
+import { COMPENDIUM } from "../../../../../constants/textsAndMessages";
 
 const CardBackCenter = () => {
-  const SPECIAL_RULES = { fontFamily: "jaapokkiRegular", paddingLeft: "0.5em" };
-
+  const theme = useTheme();
   const SC = useContext(StateCardContext);
 
   return (
     <Grid item>
-      <Typography variant="body1" align="center" sx={SPECIAL_RULES}>
-        {SC.unit.specialRules === "" ? "Keine Besonderen Spielregeln" : SC.unit.specialRules}
+      <Typography
+        variant="body1" //
+        align="center"
+        sx={theme.palette.statCards.cardBackGround}
+      >
+        {SC.unit.specialRules === "" ? COMPENDIUM.NO_SPECIAL_RULES : SC.unit.specialRules}
         {"equipment" in SC.unit && SC.unit.equipment.length !== 0
           ? SC.unit.equipment.map((e, i) => {
               return (
                 <Fragment key={i}>
                   {/* <hr></hr> */}
-                  <Typography variant="body1" sx={SPECIAL_RULES}>
+                  <Typography variant="body1" sx={CSS}>
                     {e.name}
                   </Typography>
                   <Typography>_______</Typography>
-                  <Typography variant="body1" sx={SPECIAL_RULES}>
+                  <Typography variant="body1" sx={CSS}>
                     {e.rule}
                   </Typography>
                 </Fragment>
