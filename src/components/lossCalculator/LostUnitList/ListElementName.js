056a800afe0715fa242a45d78fd0ee8d0f844fb5
marcelbade <62931394+marcelbade@users.noreply.github.com>
2024-04-17 17:29:33 +0200

    refactored style logic

--- src/components/lossCalculator/LostUnitList/ListElementName.js
+++ src/components/lossCalculator/LostUnitList/ListElementName.js
@@ -4,21 +4,27 @@ import React from "react";
 import { Typography, Grid } from "@mui/material";
 
 const ListElementName = (props) => {
+  const STYLES = {
+    paddingLeft: "1em", //
+    display: "flex",
+    justifyContent: "flex-start",
+    alignItems: "center",
+  };
+
+  const setStyles = () => {
+    return props.unitDestroyed
+      ? {
+          ...STYLES,
+          color: "red",
+          textDecorationLine: "line-through",
+          textDecorationThickness: "0.2em",
+        }
+      : STYLES;
+  };
+
   return (
     <Grid item>
-      <Typography
-        variant="h6"
-        sx={
-          props.unitDestroyed //
-            ? {
-                paddingLeft: "1em",
-                color: "red",
-                textDecorationLine: "line-through",
-                textDecorationThickness: "0.2em",
-              }
-            : { paddingLeft: "1em" }
-        }
-      >
+      <Typography variant="h6" sx={setStyles()}>
         {props.unitName}
       </Typography>
     </Grid>
