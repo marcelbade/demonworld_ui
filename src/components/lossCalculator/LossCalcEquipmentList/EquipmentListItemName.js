99cf0b69d16f0160182c0a18c2ad92f6b89a88e0
marcelbade <62931394+marcelbade@users.noreply.github.com>
2024-04-17 17:33:39 +0200

    fixed layout

--- src/components/lossCalculator/LossCalcEquipmentList/EquipmentListItemName.js
+++ src/components/lossCalculator/LossCalcEquipmentList/EquipmentListItemName.js
@@ -26,12 +26,10 @@ const EquipmentListItemName = (props) => {
 
   return props.notSingleElementItem ? (
     <Grid container direction="row" justifyContent="flex-start">
-      <Grid item xs={4}>
-        <ListItemText
-          primary={<span>{props.itemName}</span>} //
-          secondary={<span> {props.pointCost}</span>}
-        />
-      </Grid>
+      <ListItemText
+        primary={<span>{props.itemName}</span>} //
+        secondary={<span> {props.pointCost}</span>}
+      />
       <ContextHelpButton
         message={LOSS_CALCULATOR.NOT_SINGLE_ELEMENT_ITEM_MESSAGE} //
         type={PUSH_MESSAGE_TYPES.INFO}
