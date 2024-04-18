57d32f5f84da6f9c46d16862c9094c25e2eff945
marcelbade <62931394+marcelbade@users.noreply.github.com>
2024-04-18 15:31:52 +0200

    minor bug

--- src/components/ListGenerator/RightSideMenus/Menus/CardView/CardViewBox.js
+++ src/components/ListGenerator/RightSideMenus/Menus/CardView/CardViewBox.js
@@ -30,7 +30,7 @@ const CardViewBox = () => {
       <CssBaseline />
       <Grid
         container //
-        direction={COLUMN}
+        direction="column"
         sx={{ height: "100vh", width: "30vw", padding: "2em" }}
       >
         <Grid item>
