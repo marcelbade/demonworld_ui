68155f616879532028653bb1d81cfb0e4fea4313
marcelbade <62931394+marcelbade@users.noreply.github.com>
2024-04-19 13:43:33 +0200

    Changed component name.

--- src/components/ListGenerator/ArmySelectorView/ArmySelectionBox.js
+++ src/components/ListGenerator/ArmySelectorView/ArmySelectionBox.js
@@ -2,14 +2,14 @@
 import React, { Fragment } from "react";
 // components and functions
 import ArmySelectorDropdown from "./ArmySelectorDropdown";
-import AlternativeArmyLists from "./AlternativeArmyListSelection/AlternativeArmyLists";
+import AlternativeArmyListSelector from "./AlternativeArmyListSelection/AlternativeArmyListSelector";
 import FactionTreeView from "./SelectorTreeView/FactionTreeView";
 
 const ArmySelectionBox = () => {
   return (
     <Fragment>
       <ArmySelectorDropdown />
-      <AlternativeArmyLists />
+      <AlternativeArmyListSelector />
       <FactionTreeView />
     </Fragment>
   );
