d8b158f5849689f8ae53d1c738002f75c61b568c
marcelbade <62931394+marcelbade@users.noreply.github.com>
2024-04-22 10:53:03 +0200

    page number component refactored.

--- src/components/PDFGenerator/sharedPDFComponents/PageNumber.js
+++ src/components/PDFGenerator/sharedPDFComponents/PageNumber.js
@@ -3,7 +3,7 @@ import React from "react";
 // fonts
 import jaapokkiRegular from "../../../assets/fonts/jaapokkiRegular.ttf";
 // react-pdf
-import { Document, Font, Text } from "@react-pdf/renderer";
+import { Font, Text } from "@react-pdf/renderer";
 // styles
 import { commonStyles } from "../pdfStyles/commonStyles";
 
@@ -11,10 +11,6 @@ import { commonStyles } from "../pdfStyles/commonStyles";
 Font.register({ family: "jaapokkiRegular", src: jaapokkiRegular });
 
 const PageNumber = () => {
-  return (
-    <Document>
-      <Text style={commonStyles.pageNumber} render={({ pageNumber, totalPages }) => `${pageNumber}/${totalPages} `} fixed />
-    </Document>
-  );
+  return <Text style={commonStyles.pageNumber} render={({ pageNumber, totalPages }) => `${pageNumber}/${totalPages} `} fixed />;
 };
 export default PageNumber;
