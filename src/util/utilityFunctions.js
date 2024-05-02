9b2429d747bf9d210c5a6f98f4d0997376686ddf
marcelbade <62931394+marcelbade@users.noreply.github.com>
2024-05-02 02:26:53 +0200

    Removed dead code.

--- src/util/utilityFunctions.js
+++ src/util/utilityFunctions.js
@@ -3,11 +3,11 @@ import { HERO, MAGE, AUTOMATON, GIANT } from "../constants/unitTypes";
 
 /**
  * Function checks if a subFaction is an alternative sub faction.
- * If true, it is only displayed if the flag selectedAlternativeOptionis set to true too.
+ * If true, it is only displayed if the flag selectedAlternativeOption is set to true too.
  * @param {subFaction dto} subfactionDataObject
  * @returns
  */
-export const isSubFactionAlternativeAndSelective = (subfactionDataObject) => {
+export const isSubFactionAlternativeAndSelected = (subfactionDataObject) => {
   if (subfactionDataObject.alternativeListOption) {
     return subfactionDataObject.selectedAlternativeOption;
   }
@@ -63,40 +63,5 @@ export const isSingleElementCard = (unit) => {
   return SINGLE_ELEMENTS_LIST.includes(unit.unitType);
 };
 
-/**
- * This being JS, there is no way to elegantly sort an object collection by more than 2 properites without negating previous sorting. This function divides a list of unitCards by unit type, sorts each new partial array by point cost and alphabetically, then adds the resulting sorted arrays to either one of two arrays, depending on whether they have more than 1 element. Finally, these two arrays are merged into a single array and returned.
- * @param {[unitCard]} unitList
- * @returns [unitCard], sorted in this order, by number of elements (1 or more), unit type, point cost and alphabetically.
- */
-export const unitCardMultiSort = (unitList) => {
-  let types = [];
-  let singleElements = [];
-  let multipleElements = [];
-  let result = [];
-
-  // Step 1:create an array of the distinct unitTypes present in the unit list
-  unitList.forEach((u) => {
-    if (!types.includes(u.unitType)) {
-      types.push(u.unitType);
-    }
-  });
-
-  // Step 2: sort and merge into the resulting array
-  for (let i = 0; i < types.length; i++) {
-    let detailSortResult = unitList
-      .filter((u) => u.unitType === types[i])
-      .sort((a, b) => {
-        return a.points === b.points ? a.unitName > b.unitName : a.points > b.points;
-      });
-
-    if (detailSortResult[0].numberOfElements === 1) {
-      singleElements = [...singleElements, ...detailSortResult];
-    } else {
-      multipleElements = [...multipleElements, ...detailSortResult];
-    }
-  }
-
-  result = [...singleElements, ...multipleElements];
-
-  return result;
-};
+ 
+ 
