d5ff13ec8c606c57168d644acf70d5b966710534
marcelbade <62931394+marcelbade@users.noreply.github.com>
2024-05-15 16:49:15 +0200

    Completely refactored, simplofied, replaced raw strings with constants, separation of html and string concotenation.

--- src/components/PDFGenerator/detailedCardPDF/detailedCardsComponets/firstBlackRow/firstBlackRowComponents/BackSideElementsRow.js
+++ src/components/PDFGenerator/detailedCardPDF/detailedCardsComponets/firstBlackRow/firstBlackRowComponents/BackSideElementsRow.js
@@ -4,9 +4,16 @@ import React from "react";
 import { Text, View } from "@react-pdf/renderer";
 // styles
 import { detailedStyles } from "../../../../pdfStyles/detailedCardPdfStyles";
+import { CARD_PREVIEW } from "../../../../../../constants/textsAndMessages";
 
 const BackSideElementsRow = (props) => {
-  //
+  /**
+   * Function creates the string that contains the numbe of "normal" elements.
+   * Since special elements are mentioned separately,
+   * they have to be substracted from the total count.
+   * @param {unitCard} unit
+   * @returns A String with the correct number of elements and the correct phrasing.
+   */
   const numberOfElements = (unit) => {
     let specialElements = 0;
 
@@ -21,27 +28,33 @@ const BackSideElementsRow = (props) => {
     }
 
     let number = `${unit.numberOfElements - specialElements}`;
-    let ending = unit.numberOfElements === 1 ? " Element" : " Elemente";
+    let ending =
+      unit.numberOfElements === 1 //
+        ? ` ${CARD_PREVIEW.SINGLE_ELEMENT}`
+        : ` ${CARD_PREVIEW.ELEMENTS}`;
 
     return number + ending;
   };
 
+  const hasNoSpecialElements =
+    !props.unit.leader && //
+    !props.unit.standardBearer &&
+    !props.unit.musician;
+
+  const LEADER = props.unit.leader ? `${CARD_PREVIEW.LEADER} ` : null;
+  const STANDARD_BEARER = props.unit.standardBearer ? `/ ${CARD_PREVIEW.STANDARD_BEARER}` : null;
+  const MUSICIAN = props.unit.musician ? `/ ${CARD_PREVIEW.MUSICIAN}` : null;
+
   return (
     <View key={props.index} style={detailedStyles.cardBlackRow}>
-      {!props.unit.leader && !props.unit.standardBearer && !props.unit.musician ? (
-        <View style={detailedStyles.firstBlackRowBackOneElement}>
-          <Text> {numberOfElements(props.unit)} </Text>
-        </View>
-      ) : (
-        <View style={detailedStyles.firstBlackRowBackTwoElements}>
-          <Text>
-            {props.unit.leader ? "Anführer " : null}
-            {props.unit.standardBearer ? "/ Standarte" : null}
-            {props.unit.musician ? "/ Musiker" : null}{" "}
-          </Text>
-          <Text> {numberOfElements(props.unit)} </Text>
-        </View>
-      )}
+      <View style={hasNoSpecialElements ? detailedStyles.firstBlackRowBackOneElement : detailedStyles.firstBlackRowBackTwoElements}>
+        <Text>
+          {LEADER}
+          {STANDARD_BEARER}
+          {MUSICIAN}
+        </Text>
+        <Text> {numberOfElements(props.unit)} </Text>
+      </View>
     </View>
   );
 };
