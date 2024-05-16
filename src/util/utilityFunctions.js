f395f0b7e7e08b1311fa4b946bcf87676ae4641c
marcelbade <62931394+marcelbade@users.noreply.github.com>
2024-05-16 23:08:54 +0200

    Added function to utilites.

--- src/util/utilityFunctions.js
+++ src/util/utilityFunctions.js
@@ -1,4 +1,5 @@
 //constants
+import { CARD_PREVIEW } from "../constants/textsAndMessages";
 import { HERO, MAGE, AUTOMATON, GIANT } from "../constants/unitTypes";
 
 /**
@@ -74,3 +75,32 @@ export const isHeroOrMage = (unit) => {
   const HERO_MAGE_LIST = [HERO, MAGE];
   return HERO_MAGE_LIST.includes(unit.unitType);
 };
+
+  /**
+   * Function creates the string that contains the numbe of "normal" elements.
+   * Since special elements are mentioned separately,
+   * they have to be substracted from the total count.
+   * @param {unitCard} unit
+   * @returns A String with the correct number of elements and the correct phrasing.
+   */
+  export const numberOfElements = (unit) => {
+    let specialElements = 0;
+
+    if (unit.leader) {
+      ++specialElements;
+    }
+    if (unit.standardBearer) {
+      ++specialElements;
+    }
+    if (unit.musician) {
+      ++specialElements;
+    }
+
+    let number = `${unit.numberOfElements - specialElements}`;
+    let ending =
+      unit.numberOfElements === 1 //
+        ? ` ${CARD_PREVIEW.SINGLE_ELEMENT}`
+        : ` ${CARD_PREVIEW.ELEMENTS}`;
+
+    return number + ending;
+  };
