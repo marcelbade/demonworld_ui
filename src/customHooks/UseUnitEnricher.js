c776445e1860470f3b9068ae66e56807abc91553
marcelbade <62931394+marcelbade@users.noreply.github.com>
2024-05-17 15:00:30 +0200

    Moved logic to BE to enable storing army list in v2.

--- src/customHooks/UseUnitEnricher.js
+++ src/customHooks/UseUnitEnricher.js
@@ -14,47 +14,8 @@ const useUnitEnricher = () => {
     return { ...unit, uniqueID: randomID };
   };
 
-  /**
-   * Function adds a property that counts the number of lost elements the unit has suffered. Needed for the loss calculator module.
-   * @param {*} unit
-   * @returns {} unit object with a lossCounter property
-   */
-  const addLossCounterToUnit = (unit) => {
-    const max = unit.numberOfElements * unit.hitpoints;
-
-    return {
-      ...unit,
-      lossCounter: 0,
-      maxCounter: max,
-      unitDestroyed: false,
-    };
-  };
-
-  /**
-   * Function adds a property which allows equipment to be added,
-   * as well as to check what equipment can be added/ was added.
-   * There are, at this moment, 5 types of items. Each type can be selected once.
-   * @param {unitCard object} unit
-   * @returns unit object with equipment + equipmentTypes property.
-   */
-  const addEquipmentSlotsToUnit = (unit) => {
-    return {
-      ...unit,
-      equipment: [],
-      equipmentTypes: {
-        magicItem: false,
-        banner: false,
-        instrument: false,
-        unit: false,
-        fortifications: false,
-      },
-    };
-  };
-
   const enrichUnit = (unit) => {
     unit = addUniqueIdToUnit(unit);
-    unit = addLossCounterToUnit(unit);
-    unit = addEquipmentSlotsToUnit(unit);
 
     return unit;
   };
