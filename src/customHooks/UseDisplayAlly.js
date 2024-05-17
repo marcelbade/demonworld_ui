b25594be28dc5232fb24e489afeba5342f3f8793
marcelbade <62931394+marcelbade@users.noreply.github.com>
2024-05-17 15:21:28 +0200

    moved public function  to the top.

--- src/customHooks/UseDisplayAlly.js
+++ src/customHooks/UseDisplayAlly.js
@@ -21,6 +21,27 @@ const useDisplayAlly = () => {
   const AYC = useContext(AllyContext);
   const ALC = useContext(AlternativeListContext);
 
+  /**
+   * Function implements the logic whether an ally should be shown or not.
+   * @param {String} factionName
+   * @returns true, if the ally is to be displayed in the UI.
+   */
+  const showAlly = (factionName) => {
+    if (ALC.allyIsAlternativeOption) {
+      return ALC.selectedAlternativeLists.includes(AYC.allyName);
+    }
+
+    const hasAlly = AYC.allyName !== NO_ALLY;
+    return hasAlly && additionalFactionRules(factionName);
+  };
+
+  /**
+   * Function searches for additional rules for the selected factuion. if found they are called and
+   * the resulting boolean flag is returned.
+   * @param {String} factionName
+   * @returns either additional rules for this faction that return a boolean flag, or true, if no
+   * further rules exist for the selected faction.
+   */
   const additionalFactionRules = (factionName) => {
     switch (factionName) {
       case EMPIRE_TEXTS.FACTION_NAME:
@@ -41,13 +62,6 @@ const useDisplayAlly = () => {
       ALC.selectedAlternativeLists //
         .includes(EMPIRE_TEXTS.SF.SOUTH_MARCH)
     );
-  };
-
-  const showAlly = (factionName) => {
-    if (ALC.allyIsAlternativeOption) {
-      return ALC.selectedAlternativeLists.includes(AYC.allyName);
-    }
-    return AYC.allyName !== NO_ALLY && additionalFactionRules(factionName);
   };
 
   return { showAlly: showAlly };
