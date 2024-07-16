de0c8228a2cd7eaecf5ddc25a1724c59a84980f9
marcelbade <62931394+marcelbade@users.noreply.github.com>
2024-07-16 16:08:13 +0200

    Added movement summons.

--- src/components/PDFGenerator/detailedCardPDF/detailedCardsComponets/firstBlackRow/firstBlackRowComponents/UnitMovementRow.js
+++ src/components/PDFGenerator/detailedCardPDF/detailedCardsComponets/firstBlackRow/firstBlackRowComponents/UnitMovementRow.js
@@ -3,7 +3,6 @@ import React from "react";
 // react-pdf
 import { Text, View, Image } from "@react-pdf/renderer";
 // functions and components
-import { isHeroOrMage, isSingleElementCard } from "../../../../../../util/utilityFunctions";
 import { CARD_TEXT } from "../../../../../../constants/textsAndMessages";
 // icons
 import squareFormationWhite from "../../../../../../assets/icons/squareFormationWhite.png";
@@ -11,6 +10,7 @@ import skirmishFormation from "../../../../../../assets/icons/skirmishFormation.
 import wedgeFormation from "../../../../../../assets/icons/wedgeFormation.png";
 // styles
 import { detailedStyles } from "../../../../pdfStyles/detailedCardPdfStyles";
+import { MAGE, HERO, GIANT, UNIT, SUMMONED, AUTOMATON } from "../../../../../../constants/unitTypes";
 
 const UnitMovementRow = (props) => {
   const HERO_MAGE_MOVEMENT = `${props.unit.move} ${CARD_TEXT.MOVEMENT_POINTS}`;
@@ -35,40 +35,59 @@ const UnitMovementRow = (props) => {
   ) : null;
 
   const HORDE_FORMATION = props.unit.horde ? CARD_TEXT.HORDE : null;
-  const OVERRUN = `${CARD_TEXT.OVERRUN}: ${props.unit.overRun}` > 0;
 
-  console.log(`${CARD_TEXT.OVERRUN}: ${props.unit.overRun}`);
+  const CONTROLZONE = `${CARD_TEXT.CONTROL_AREA}: ${props.unit.controlZone}`;
+
+  const OVERRUN_LARGE = props.unit.overRun > 0 ? `${CARD_TEXT.OVERRUN}: ${props.unit.overRun}` : null;
+
+  // large elements
+  const MOVEMENT_LARGE =
+    `${CARD_TEXT.MOVE}: ${props.unit.move} ` +
+    `/ ${CARD_TEXT.CHARGE}: ${props.unit.charge} ` +
+    `/ ${CARD_TEXT.SKIRMISH}: ${props.unit.skirmish} ` +
+    `/ ${CARD_TEXT.HOLD}: ${props.unit.hold_maneuvers}`;
 
   /**
    * Function decides whether the formation mut be displayed by checking if one of the properties is not null.
    * @returns true, if any of the formation properties is not null
    */
-  return isSingleElementCard(props.unit) && isHeroOrMage(props.unit) ? (
+  return (
     <View
       key={props.index} //
       style={detailedStyles.cardUpperBlackRow}
     >
-      <Text key={props.index}>{HERO_MAGE_MOVEMENT}</Text>
-    </View>
-  ) : (
-    <View
-      key={props.index} //
-      style={detailedStyles.cardUpperBlackRow}
-    >
-      <Text key={props.index}>{UNIT_MOVEMENT}</Text>
-      <Text key={props.index}>{MANEUVERS}</Text>
-      <View
-        key={props.index} //
-        style={detailedStyles.formations}
-      >
-        {OVERRUN}
-        {SKIRMISH_FORMATION}
-        {SQUARE_FORMATION}
-        {WEDGE_FORMATION}
-        {HORDE_FORMATION}
-
-        <Text key={props.index}> {HORDE_FORMATION}</Text>
-      </View>
+      {props.unit.unitType === HERO || props.unit.unitType === MAGE ? (
+        <View style={detailedStyles.cardUpperBlackRowVariant}>
+          <Text style={detailedStyles.movementText} key={props.index}>
+            {HERO_MAGE_MOVEMENT}
+          </Text>
+          <Text> {props.unit.controlZone > 1 ? <Text variant="h6">{CONTROLZONE}</Text> : null}</Text>
+        </View>
+      ) : null}
+      {props.unit.unitType === GIANT || props.unit.unitType === AUTOMATON ? (
+        <View style={detailedStyles.cardUpperBlackRowVariant}>
+          <Text style={detailedStyles.movementText} variant="h6">
+            {MOVEMENT_LARGE}
+          </Text>
+          <Text style={detailedStyles.movementText} variant="h6">
+            {OVERRUN_LARGE}
+          </Text>
+        </View>
+      ) : null}
+      {props.unit.unitType === UNIT || props.unit.unitType === SUMMONED ? (
+        <View style={detailedStyles.cardUpperBlackRowVariant}>
+          <Text style={detailedStyles.movementText} key={props.index}>
+            {UNIT_MOVEMENT}
+          </Text>
+          <Text style={detailedStyles.movementText} key={props.index}>
+            {MANEUVERS}
+          </Text>
+          {SKIRMISH_FORMATION}
+          {SQUARE_FORMATION}
+          {WEDGE_FORMATION}
+          {HORDE_FORMATION}
+        </View>
+      ) : null}
     </View>
   );
 };
