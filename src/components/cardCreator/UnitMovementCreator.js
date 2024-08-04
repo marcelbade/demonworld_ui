0f2d7661b4437d774086cab547603ef4a6955be5
marcelbade <62931394+marcelbade@users.noreply.github.com>
2024-08-04 12:06:21 +0200

    added parse int to number fields.

--- src/components/cardCreator/UnitMovementCreator.js
+++ src/components/cardCreator/UnitMovementCreator.js
@@ -1,47 +1,40 @@
 // react
 import React, { Fragment, useContext } from "react";
 // material ui
-import { Grid, Typography } from "@mui/material";
+import { Grid, Typography, FormControlLabel, Checkbox } from "@mui/material";
 import CreatorTextInput from "./CreatorTextInput";
 // components and functions
 import FormationsAndHordeCreator from "./FormationsAndHordeCreator";
 // contexts
 import { CardCreationContext } from "../../contexts/cardCreationContext";
+// constants
 import { CREATOR } from "../../constants/textsAndMessages";
+import { AUTOMATON, GIANT, HERO, MAGE, SUMMONED, UNIT } from "../../constants/unitTypes";
 
 const UnitMovementCreator = () => {
   const CCC = useContext(CardCreationContext);
 
-  const deleteMovement = () => {
-    CCC.setMove("");
-  };
-
   const changeMovement = (event) => {
-    CCC.setMove(event.target.value);
-  };
-
-  const deleteSkirmish = () => {
-    CCC.setMove("");
+    CCC.setMove(parseInt(event.target.value));
   };
 
   const changeSkirmish = (event) => {
-    CCC.setSkirmish(event.target.value);
-  };
-
-  const deleteCharge = () => {
-    CCC.setMove("");
+    CCC.setSkirmish(parseInt(event.target.value));
   };
 
   const changeCharge = (event) => {
-    CCC.setCharge(event.target.value);
-  };
-
-  const deleteManeuver = () => {
-    CCC.setManeuver("");
+    CCC.setCharge(parseInt(event.target.value));
   };
 
   const changeManeuver = (event) => {
-    CCC.setManeuver(event.target.value);
+    CCC.setManeuver(parseInt(event.target.value));
+  };
+
+  const changeHasMaxFieldsMove = () => {
+    CCC.setHasMaxFieldsMove((prevState) => !prevState);
+  };
+  const renderSummonsMovLabelText = () => {
+    return CCC.hasMaxFieldsMove ? CREATOR.MAX_MOVE_POINTS : CREATOR.MOVMENT_POINTS;
   };
 
   const inputElements = [
@@ -49,35 +42,73 @@ const UnitMovementCreator = () => {
       id: "move", //
       label: "",
       value: CCC.move,
-      onClick: changeManeuver,
       onChange: changeMovement,
       statName: CREATOR.MOVE,
+      display: CCC.unitType === UNIT || CCC.unitType === GIANT || CCC.unitType === AUTOMATON,
     },
     {
       id: "skirmish", //
       label: "",
       value: CCC.skirmish,
-      onClick: changeManeuver,
       onChange: changeSkirmish,
       statName: CREATOR.SKIRMISH,
+      display: CCC.unitType === UNIT || CCC.unitType === GIANT || CCC.unitType === AUTOMATON,
     },
     {
       id: "charge", //
       label: "",
       value: CCC.charge,
-      onClick: changeCharge,
       onChange: changeCharge,
       statName: CREATOR.CHARGE,
+      display: CCC.unitType === UNIT || CCC.unitType === GIANT || CCC.unitType === AUTOMATON,
     },
     {
       id: "maneuver", //
       label: "",
       value: CCC.maneuver,
-      onClick: changeManeuver,
+
       onChange: changeManeuver,
       statName: CREATOR.MANEUVER,
+      display: CCC.unitType === UNIT,
+    },
+    {
+      id: "movementPoints", //
+      label: "",
+      value: CCC.move,
+
+      onChange: changeMovement,
+      statName: CREATOR.MOVMENT_POINTS,
+      display: CCC.unitType === HERO || CCC.unitType === MAGE,
+    },
+    {
+      id: "hold", //
+      label: "",
+      value: CCC.maneuver,
+
+      onChange: changeManeuver,
+      statName: CREATOR.HOLD,
+      display: CCC.unitType === GIANT || CCC.unitType === AUTOMATON,
+    },
+    {
+      id: "maxMovementPoints", //
+      label: "",
+      value: CCC.move,
+
+      onChange: changeMovement,
+      statName: renderSummonsMovLabelText(),
+      display: CCC.unitType === SUMMONED,
     },
   ];
+
+  const renderSeparator = (iterator) => {
+    const MAX_STAT_NUMBER = 3;
+
+    if (CCC.unitType === HERO || CCC.unitType === MAGE || CCC.unitType === SUMMONED) {
+      return false;
+    }
+
+    return iterator < MAX_STAT_NUMBER;
+  };
 
   return (
     <Grid
@@ -93,8 +124,7 @@ const UnitMovementCreator = () => {
         borderRadius: "10px",
       }}
     >
-      <FormationsAndHordeCreator />
-
+      {CCC.unitType === UNIT ? <FormationsAndHordeCreator /> : null}
       <Grid
         container //
         item
@@ -102,33 +132,47 @@ const UnitMovementCreator = () => {
         alignItems="center" //
         justifyContent="center"
       >
-        {inputElements.map((input, i) => (
-          <Fragment key={input.id}>
-            <CreatorTextInput
-              id={input.id}
-              value={input.value}
-              onClick={input.onClick}
-              onChange={input.onChange}
-              label={input.statName}
-              width={"7em"}
-            />
-
-            {i < 3 ? (
-              <Typography
-                variant="h3" //
-                sx={{
-                  color: "white", //
-                  marginLeft: "1em",
-                  marginRight: "0.5em",
-                }}
-                key={i}
-              >
-                /
-              </Typography>
-            ) : null}
-          </Fragment>
-        ))}
+        {inputElements
+          .filter((input) => input.display)
+          .map((input, i) => (
+            <Fragment key={input.id}>
+              <CreatorTextInput
+                id={input.id}
+                value={input.value}
+                onClick={input.onClick}
+                onChange={input.onChange}
+                label={input.statName}
+                width="7em"
+              />
+              {/*  dont't add a "/" after the last element */}
+              {renderSeparator(i) ? (
+                <Typography
+                  variant="h3" //
+                  sx={{
+                    color: "white",
+                    marginLeft: "1em",
+                    marginRight: "0.5em",
+                  }}
+                  key={i}
+                >
+                  /
+                </Typography>
+              ) : null}
+            </Fragment>
+          ))}
       </Grid>
+      {CCC.unitType === SUMMONED ? (
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={CCC.hasMaxFieldsMove} //
+              onChange={changeHasMaxFieldsMove}
+            />
+          }
+          label={CREATOR.MAX_MOVE_POINTS}
+          labelPlacement="start"
+        />
+      ) : null}
     </Grid>
   );
 };
