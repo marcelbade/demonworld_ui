33c45013a3ec9c6de004afd033307d9f46753ae4
marcelbade <62931394+marcelbade@users.noreply.github.com>
2024-10-05 01:04:22 +0200

    Refactored Stat Card component.

--- src/components/shared/statCards/cardComponents/cardBackComponents/CardBackLowerBlackStripe.js
+++ src/components/shared/statCards/cardComponents/cardBackComponents/CardBackLowerBlackStripe.js
@@ -1,20 +1,18 @@
 // React
-import React, { useContext } from "react";
+import React from "react";
 // Material UI
 import { Grid, Typography } from "@mui/material";
 import { useTheme } from "@emotion/react";
 // functions and modules
-import { StateCardContext } from "../../../../../contexts/statCardContext";
 import usePointCostCalculator from "../../../../../customHooks/UsePointCostCalculator";
 // constants
 import { STATS } from "../../../../../constants/textsAndMessages";
 
-const CardBackLowerBlackStripe = () => {
+const CardBackLowerBlackStripe = (props) => {
   const calculator = usePointCostCalculator();
   const theme = useTheme();
 
-  const SC = useContext(StateCardContext);
-  const totalPoints = `${calculator.calculateTotalUnitCost(SC.unit)} ${STATS.POINTS}`;
+  const totalPoints = `${calculator.calculateTotalUnitCost(props.unit)} ${STATS.POINTS}`;
 
   return (
     <Grid>
