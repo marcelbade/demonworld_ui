da61d3efed0236b638bb13d53de71ba605688046
marcelbade <62931394+marcelbade@users.noreply.github.com>
2025-02-10 13:31:12 +0100

    Fixed bug: empty subfactions must be displayed in the army list, if they have a minimum point limit.

--- src/components/ListGenerator/ArmyListView/ArmyList/ArmyListCenter/ArmyListComponents/ArmyListSubFactionEntry.js
+++ src/components/ListGenerator/ArmyListView/ArmyList/ArmyListCenter/ArmyListComponents/ArmyListSubFactionEntry.js
@@ -26,19 +26,29 @@ const ArmyListSubFactionEntry = (props) => {
     }
   };
 
+  /**
+   * Function checks if a sub Faction should be displayed in the army list
+   * @returns true, if the subFaction either is not empty or is blow the minimum points
+   * needed to make the list valid.
+   */
+  const displaySubFaction = () => {
+    return props.units.length !== 0 || !props.valid;
+  };
+
   return (
     <ListItem //
       key="subfactionEntry"
-      sx={props.units.length === 0 && !fadeAwayFlag ? { display: "none" } : { display: "block" }}
+      sx={displaySubFaction() ? { display: "block" } : { display: "none" }}
     >
       <Grid
         container //
         direction="column"
-        sx={
-          fadeAwayFlag //
+        sx={{
+          ...(fadeAwayFlag //
             ? theme.palette.animation.fadeAway
-            : theme.palette.animation.fadeIn
-        }
+            : theme.palette.animation.fadeIn),
+          minWidth: "30em",
+        }}
       >
         {LDC.simpleModeOn ? null : (
           <ArmyListSubFactionHeader
