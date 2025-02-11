58ffc403705ba59307a27e6c97cf812327896595
marcelbade <62931394+marcelbade@users.noreply.github.com>
2025-02-11 19:49:28 +0100

    Deleted unnecessary context.

--- src/contexts/validationContext.js
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-
-const ValidationContext = createContext();
-const ValidationProvider = ValidationContext.Provider;
-
-export { ValidationContext };
-export default ValidationProvider;
