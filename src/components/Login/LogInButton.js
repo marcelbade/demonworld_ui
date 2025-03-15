486eeaacb4ac8739769e6b66ce94fcee85adda42
marcelbade <62931394+marcelbade@users.noreply.github.com>
2025-03-15 23:10:36 +0100

    Added new icon, continued working on login button

--- src/components/Login/LogInButton.js
+++ src/components/Login/LogInButton.js
@@ -1,12 +1,12 @@
 // React
 import React, { useContext } from "react";
 // Material UI
-import { IconButton, Tooltip, Typography } from "@mui/material";
+import { Avatar, IconButton, Tooltip, Typography } from "@mui/material";
 // icons
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import FaceIcon from "@mui/icons-material/Face";
 // functions and components
-import { LOGIN } from "../../constants/textsAndMessages";
+import { USER_AUTH } from "../../constants/textsAndMessages";
 // contexts
 import { UserContext } from "../../contexts/userContext";
 
@@ -17,23 +17,47 @@ const LogInButton = (props) => {
     UC.setDisplayLogInPrompt(true);
   };
 
+  const Logout = () => {};
+
   return (
-    <Tooltip title={<Typography>{LOGIN.LOGIN_BUTTON}</Typography>}>
-      <IconButton
-        sx={{
-          width: props.buttonWidth,
-          height: props.buttonHeight,
-        }}
-        onClick={() => {
-          displayLogInPrompt();
-        }}
-      >
-        {UC.userLoggedIn ? ( //
-          <FaceIcon fontSize={props.iconSize} />
-        ) : (
+    <Tooltip
+      title={
+        <Typography>
+          {UC.userLoggedIn //
+            ? USER_AUTH.LOGOUT_BUTTON
+            : USER_AUTH.LOGIN_BUTTON}
+        </Typography>
+      }
+    >
+      {UC.userLoggedIn ? (
+        <IconButton
+          sx={{
+            width: props.buttonWidth,
+            height: props.buttonHeight,
+          }}
+          onClick={() => {
+            Logout();
+          }}
+        >
+          <Avatar sx={{ backgroundColor: "green" }}>
+            {UC.user.userName //
+              .charAt(0)
+              .toUpperCase()}
+          </Avatar>
+        </IconButton>
+      ) : (
+        <IconButton
+          sx={{
+            width: props.buttonWidth,
+            height: props.buttonHeight,
+          }}
+          onClick={() => {
+            displayLogInPrompt();
+          }}
+        >
           <AccountCircleIcon fontSize={props.iconSize} />
-        )}
-      </IconButton>
+        </IconButton>
+      )}
     </Tooltip>
   );
 };
