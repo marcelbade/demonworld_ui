46cffe546d020bd166b8f1f339c07f7a0ab8841d
marcelbade <62931394+marcelbade@users.noreply.github.com>
2025-03-15 13:39:08 +0100

    Created file for urls.

---
+++ src/constants/URLs.js
@@ -0,0 +1,13 @@
+const PREFIX_BE = "http://localhost:8080";
+const PREFIX_FE = "http://localhost:3000";
+
+// users
+export const LOGIN_USER_URL = `${PREFIX_BE}/public/user/login`;
+
+// game data
+export const FACTION_DTOS_URL = `${PREFIX_BE}/public/game/factionDTOs`;
+export const ITEM_DTOS_URL = `${PREFIX_BE}/public/game/itemDTOs`;
+export const ALL_UNITS_URL = `${PREFIX_BE}/public/game/allUnits`;
+
+// pdf
+export const PDF_URL = `${PREFIX_FE}/PdfBox`;
