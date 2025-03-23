4645029ccb2b7e23414a6a2371799cb556f03e07
marcelbade <62931394+marcelbade@users.noreply.github.com>
2025-03-23 15:48:11 +0100

    Added URL.

--- src/constants/URLs.js
+++ src/constants/URLs.js
@@ -3,6 +3,10 @@ const PREFIX_FE = "http://localhost:3000";
 
 // users
 export const LOGIN_USER_URL = `${PREFIX_BE}/public/user/login`;
+export const REGISTER_USER_URL = `${PREFIX_BE}/public/user/register`;
+
+
+
 export const ALL_USER_NAMES = `${PREFIX_BE}/public/user/allUserNames`;
 
 // game data
@@ -11,7 +15,7 @@ export const ITEM_DTOS_URL = `${PREFIX_BE}/public/game/itemDTOs`;
 export const ALL_UNITS_URL = `${PREFIX_BE}/public/game/allUnits`;
 
 //custom
-export const CREATE_CUSTOM_UNIT_URL = `${PREFIX_FE}/auth/user/createCustomUnit`;
+export const CREATE_CUSTOM_UNIT_URL = `${PREFIX_BE}/auth/user/createCustomUnit`;
 
 // pdf
 export const PDF_URL = `${PREFIX_FE}/PdfBox`;
