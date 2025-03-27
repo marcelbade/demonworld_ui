1f3074eee4629e6d399ca571b9b5d51f70152ed1
marcelbade <62931394+marcelbade@users.noreply.github.com>
2025-03-27 11:55:04 +0100

    Added comment.

--- src/components/shared/ContextHelpButton.js
+++ src/components/shared/ContextHelpButton.js
@@ -7,6 +7,15 @@ import HelpIcon from "@mui/icons-material/Help";
 //  components and functions
 import usePushMessages from "../../customHooks/UsePushMessages";
 
+/**
+ * Component displays an IconButton with a question mark. The Click action displays a notistack push message.
+ * The type attribute controls message's color (green|blue|red).
+ * API:
+ *  message: String,
+ *  type: PUSH_MESSAGE_TYPES 
+ * @param {String } props
+ * @returns
+ */
 const ContextHelpButton = (props) => {
   const pushMessages = usePushMessages();
 
