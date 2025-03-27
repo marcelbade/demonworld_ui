d6a27d19b44abd85a30cabdf13bed6916cf25bb7
marcelbade <62931394+marcelbade@users.noreply.github.com>
2025-03-27 11:56:12 +0100

    Renamed and added URLs.

--- src/constants/URLs.js
+++ src/constants/URLs.js
@@ -4,17 +4,20 @@ const PREFIX_FE = "http://localhost:3000";
 // users
 export const LOGIN_USER_URL = `${PREFIX_BE}/public/user/login`;
 export const REGISTER_USER_URL = `${PREFIX_BE}/public/user/register`;
+export const ALL_USER_NAMES_URL = `${PREFIX_BE}/public/user/allUserNames`;
 
-
-
-export const ALL_USER_NAMES = `${PREFIX_BE}/public/user/allUserNames`;
+//store army list
+export const STORE_ARMY_LIST_URL = `${PREFIX_BE}/auth/armyLists/addList`;
 
 // game data
 export const FACTION_DTOS_URL = `${PREFIX_BE}/public/game/factionDTOs`;
 export const ITEM_DTOS_URL = `${PREFIX_BE}/public/game/itemDTOs`;
 export const ALL_UNITS_URL = `${PREFIX_BE}/public/game/allUnits`;
 
-//custom
+// events
+export const GET_EVENTS_URL = `${PREFIX_BE}/auth/event/allEvents`;
+
+//custom units and items
 export const CREATE_CUSTOM_UNIT_URL = `${PREFIX_BE}/auth/user/createCustomUnit`;
 
 // pdf
