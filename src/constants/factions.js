704413dac3ffe61679a01773e0841219666be303
marcelbade <62931394+marcelbade@users.noreply.github.com>
2025-03-28 10:37:39 +0100

    Added faction colors.

--- src/constants/factions.js
+++ src/constants/factions.js
@@ -49,6 +49,25 @@ export const ALL_FACTIONS_ARRAY = [
   UNDEAD,
 ];
 
+// ALL FACTIONS
+export const FACTION_COLORS = () => {
+  let factionColors = new Map();
+  factionColors.set(DARKELVES, "rgb(182, 61, 194)");
+  factionColors.set(DWARVES, "rgb(168, 124, 83)");
+  factionColors.set(ELVES, "rgb(35, 222, 212)");
+  factionColors.set(GOBLINS, "rgb(21, 194, 76)");
+  factionColors.set(LIZARDKIN, "rgb(199, 146, 117)");
+  factionColors.set(EMPIRE, "rgb(222, 35, 35)");
+  factionColors.set(ISHTAK, "rgb(93, 159, 181)");
+  factionColors.set(NORWINGER, "rgb(114, 179, 214)");
+  factionColors.set(ORKS, "rgb(162, 184, 82)");
+  factionColors.set(SOUTHERN_CITY_STATES, "rgb(240, 188, 37)");
+  factionColors.set(THAIN, "rgb(255, 138, 3)");
+  factionColors.set(UNDEAD, "rgb(46, 27, 6)");
+
+  return factionColors;
+};
+
 export const ALTERNATIVE_ARMY_SELECTION_TEXT = {
   [DWARVES]: [
     DWARF_TEXTS.SELECTION_TEXTS.FOURTY_PERCENT, //
