48cb2d7e7f4f9b81bbc53d1a10611276dca9b43e
marcelbade <62931394+marcelbade@users.noreply.github.com>
2025-07-09 14:35:18 +0200

    Refactored name and moved conditional display logic into the component.

--- src/components/PDFGenerator/detailedCardPDF/detailedCardsComponets/center/CardCenter.js
+++ src/components/PDFGenerator/detailedCardPDF/detailedCardsComponets/center/CardCenter.js
@@ -7,27 +7,23 @@ import { detailedStyles } from "../../../pdfStyles/detailedCardPdfStyles";
 import { commonStyles } from "../../../pdfStyles/commonStyles";
 
 // pdf components
-import RangeWeaponRow from "./cardCenterComponents/RangedWeaponRow";
+import RangedWeaponRow from "./cardCenterComponents/RangedWeaponRow";
 import MeleeWeaponRow from "./cardCenterComponents/MeleeWeaponRow";
 import SizeArmorSkill from "./cardCenterComponents/SizeArmorSkillRow";
 import BackSideRulesRow from "./cardCenterComponents/BackSideRulesRow";
 import InitiativeRow from "./cardCenterComponents/InitiativeRow";
 import ChargeBonusRow from "./cardCenterComponents/ChargeBonusRow";
-// constants
-import { NO_RANGE_WEAPON } from "../../../../../constants/textsAndMessages";
 
 const CardCenter = (props) => {
   return (
     <View style={commonStyles.table}>
       <View style={detailedStyles.tableRowDetailedList}>
         <View style={detailedStyles.cell}>
-          {props.unit.rangedWeapon === NO_RANGE_WEAPON ? null : (
-            <RangeWeaponRow
-              weaponName={props.unit.rangedWeapon} //
-              rangedAttackStats={props.unit.rangedAttackStats}
-              index={props.index}
-            />
-          )}
+          <RangedWeaponRow
+            weaponName={props.unit.rangedWeapon} //
+            rangedAttackStats={props.unit.rangedAttackStats}
+            index={props.index}
+          />
           <MeleeWeaponRow
             unit={props.unit} //
           />
