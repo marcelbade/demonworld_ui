1907a7c643fb062a7a9d04513d760657e91603bb
marcelbade <62931394+marcelbade@users.noreply.github.com>
2025-07-17 23:56:42 +0200

    Added comment.

--- src/customHooks/UsePushMessages.js
+++ src/customHooks/UsePushMessages.js
@@ -9,6 +9,16 @@ import { PUSH_MESSAGE_TYPES } from "../constants/textsAndMessages";
 const usePushMessages = () => {
   const { enqueueSnackbar, closeSnackbar } = useSnackbar();
 
+  /**
+   * Function invokes a toast message (push message) when called. 
+   * The Message will display the passed text and have the color 
+   * of the passed message type:
+   * - ERROR: red
+   * - INFO: blue
+   * - SUCCESS: green
+   * @param {String} text - any message as String
+   * @param {String} messageType - one of the three enums 
+   */
   const showSnackBar = (text, messageType) => {
     enqueueSnackbar(text, {
       persist: messageType === PUSH_MESSAGE_TYPES.ERROR ? false : true,
