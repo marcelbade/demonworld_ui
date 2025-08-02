1be11ff2ad7ba24d799d347ae88869183f861e6f
marcelbade <62931394+marcelbade@users.noreply.github.com>
2025-08-02 13:08:55 +0200

    Removed superflous url.

--- src/constants/URLs.js
+++ src/constants/URLs.js
@@ -8,7 +8,6 @@ export const ALL_USER_NAMES_URL = `${PREFIX_BE}/public/user/allUserNames`;
 
 // army lists
 export const STORE_ARMY_LIST_URL = `${PREFIX_BE}/auth/armyLists/addList`;
-export const UPDATE_ARMY_LIST_URL = `${PREFIX_BE}/auth/armyLists/updateList`;
 
 export const RETREIVE_ARMY_LIST_URL = (user) => `${PREFIX_BE}/auth/armyLists/getListsForUser?userName=${user}`;
 
@@ -22,7 +21,7 @@ export const ALL_UNITS_URL = `${PREFIX_BE}/public/game/allUnits`;
 // events
 export const GET_EVENTS_URL = `${PREFIX_BE}/auth/event/allEvents`;
 
-//custom units and items
+// custom units and items
 export const CREATE_CUSTOM_UNIT_URL = `${PREFIX_BE}/auth/user/createCustomUnit`;
 
 // pdf
