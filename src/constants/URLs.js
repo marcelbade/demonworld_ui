0d35707267e4a61a70577f167b27b6db6427e88b
marcelbade <62931394+marcelbade@users.noreply.github.com>
2025-08-05 13:37:48 +0200

    Fixed typo.

--- src/constants/URLs.js
+++ src/constants/URLs.js
@@ -9,7 +9,7 @@ export const ALL_USER_NAMES_URL = `${PREFIX_BE}/public/user/allUserNames`;
 // army lists
 export const STORE_ARMY_LIST_URL = `${PREFIX_BE}/auth/armyLists/addList`;
 
-export const RETREIVE_ARMY_LIST_URL = (user) => `${PREFIX_BE}/auth/armyLists/getListsForUser?userName=${user}`;
+export const RETRIEVE_ARMY_LISTS_URL = (user) => `${PREFIX_BE}/auth/armyLists/getListsForUser?userName=${user}`;
 
 export const DELETE_ARMY_LIST_URL = (userName, listId) => `${PREFIX_BE}/auth/armyLists/deleteList?userName=${userName}&listId=${listId}`;
 
