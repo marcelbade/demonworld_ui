aba79e5486a7b672747f5b107f22acf751553842
marcelbade <62931394+marcelbade@users.noreply.github.com>
2025-08-10 10:42:46 +0200

    Changed Text.

--- src/constants/textsAndMessages.js
+++ src/constants/textsAndMessages.js
@@ -665,9 +665,9 @@ export const SPECIAL_ITEMS = {
 
 export const CREATOR = {
   NEW_FACTION: "Neue Fraktion Erstellen",
-  FACTION_NAME: "Fraktion:",
+  FACTION_NAME: "Fraktion",
   SUBFACTION_NAME: "Teilfraktion",
-  ALLY: "Allierter:",
+  ALLY: "Allierter",
   UNIT_NAME: "Name:",
   IS_UNIQUE: "Einheit ist einzigartig.",
   IS_CAVALERY: "Einheit ist beritten.",
