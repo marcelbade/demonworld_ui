eb7325e99c9fec5ccc41b78fedf4144b56267e86
marcelbade <62931394+marcelbade@users.noreply.github.com>
2025-08-11 17:38:41 +0200

    Added custom hook.

---
+++ src/customHooks/UseConfirmationDialogSettings.js
@@ -0,0 +1,65 @@
+// context
+import { useContext } from "react";
+import { MenuContext } from "../contexts/MenuContext";
+// custom hooks
+import useAxios from "./UseAxios";
+// constants
+import { SET_OVERRIDE_DIALOG_URL } from "../constants/URLs";
+import { CONFIRMATION_DIALOG } from "../constants/textsAndMessages";
+
+const useConfirmationDialogSettings = () => {
+  const MC = useContext(MenuContext);
+
+  const callAxios = useAxios();
+
+  const setOverrideDialogSetting = () => {
+    MC.setblockDialog({
+      ...MC.blockDialog,
+      showOverrideDialog: !MC.blockDialog.showOverrideDialog,
+    });
+
+    storeOverrideDialogSetting();
+  };
+
+  const setDeletetionDialogSetting = () => {
+    MC.setblockDialog({
+      ...MC.blockDialog,
+      showDeletionDialog: !MC.blockDialog.showDeletionDialog,
+    });
+
+    storeDeletionDialogSetting();
+  };
+
+  const storeOverrideDialogSetting = () => {
+    callAxios.storeData(
+      JSON.stringify({
+        userName: UC.user.userName,
+        displayOverrideConfirmation: !MC.blockDialog.showOverrideDialog,
+      }),
+      SET_OVERRIDE_DIALOG_URL,
+      null,
+      CONFIRMATION_DIALOG.PUSH_MESSAGE
+    );
+  };
+
+  const storeDeletionDialogSetting = () => {
+    callAxios.storeData(
+      JSON.stringify({
+        userName: UC.user.userName,
+        displayDeletionConfirmation: !MC.blockDialog.showDeletionDialog,
+      }),
+      SET_DELETE_DIALOG_URL,
+      null,
+      CONFIRMATION_DIALOG.PUSH_MESSAGE
+    );
+  };
+
+  return {
+    showOverrideDialog: MC.blockDialog.showOverrideDialog,
+    showDeletionDialog: MC.blockDialog.showDeletionDialog,
+    setOverrideDialogSetting: setOverrideDialogSetting,
+    setDeletetionDialogSetting: setDeletetionDialogSetting,
+  };
+};
+
+export default useConfirmationDialogSettings;
