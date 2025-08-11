f9a3bf075b22b99d3130ecb45950180cdc19bf71
marcelbade <62931394+marcelbade@users.noreply.github.com>
2025-08-11 17:37:28 +0200

    Refactored light switch.

--- src/components/shared/LightSwitch.js
+++ src/components/shared/LightSwitch.js
@@ -1,47 +1,59 @@
 // React
-import React, { useContext } from "react";
+import { useContext } from "react";
 // Material UI
-import { IconButton, Tooltip, Typography } from "@mui/material";
+import { Grid2 as Grid, FormGroup, FormControlLabel, Switch } from "@mui/material";
 // components and functions
 import { LightSwitchContext } from "../../contexts/lightSwitchContext";
 // icons
 import BrightnessHighIcon from "@mui/icons-material/BrightnessHigh";
 import Brightness4Icon from "@mui/icons-material/Brightness4";
 // constants
-import { OPTIONS } from "../../constants/textsAndMessages";
+// import { OPTIONS } from "../../constants/textsAndMessages";
 
 const LightSwitch = (props) => {
   const LC = useContext(LightSwitchContext);
 
-  const style_light = { color: "black" };
-  const style_dark = { color: "white" };
+  const style_light = { marginTop: "0.3em", color: "black" };
+  const style_dark = { marginTop: "0.3em", color: "white" };
 
   const toggleDarkMode = () => {
     LC.setDarkModeOff((prevState) => !prevState);
   };
 
   return (
-    <Tooltip title={<Typography>{OPTIONS.LIGHT_SWITCH}</Typography>}>
-      <IconButton
-        width={ props.width}
-        height={props.height}
-        onClick={() => {
-          toggleDarkMode();
-        }}
-      >
-        {LC.darkModeOff ? (
-          <Brightness4Icon
-            fontSize={props.iconSize} //
-            sx={style_light}
-          />
-        ) : (
-          <BrightnessHighIcon
-            fontSize={props.iconSize} //
-            sx={style_dark}
-          />
-        )}
-      </IconButton>
-    </Tooltip>
+    <Grid
+      container //
+      direction="row"
+      alignContent="center"
+    >
+      {/* TODO turn the tool tip into normal text */}
+      {/* <Tooltip title={<Typography>{OPTIONS.LIGHT_SWITCH}</Typography>}>  */}
+
+      <FormGroup>
+        <FormControlLabel
+          control={
+            <Switch //
+              id="toggleAllButtons"
+              color="error"
+              checked={LC.darkModeOff}
+              onChange={toggleDarkMode}
+            />
+          }
+        />
+      </FormGroup>
+
+      {LC.darkModeOff ? (
+        <Brightness4Icon
+          fontSize={props.iconSize} //
+          sx={style_light}
+        />
+      ) : (
+        <BrightnessHighIcon
+          fontSize={props.iconSize} //
+          sx={style_dark}
+        />
+      )}
+    </Grid>
   );
 };
 
