7d97096efe03247a2d11a3ff035bc83a7511cfaf
marcelbade <62931394+marcelbade@users.noreply.github.com>
2025-08-18 14:13:39 +0200

    Added ttitles.

--- src/components/shared/options/AppOptions.js
+++ src/components/shared/options/AppOptions.js
@@ -1,6 +1,7 @@
-import { Divider, Grid2 as Grid } from "@mui/material";
+import { Divider, Grid2 as Grid, Typography } from "@mui/material";
 import CompendiumTableColOptions from "./CompendiumTableColOptions";
 import LightSwitch from "../LightSwitch";
+import { OPTIONS } from "../../../constants/textsAndMessages";
 
 const AppOptions = () => {
   return (
@@ -11,8 +12,12 @@ const AppOptions = () => {
         height: "100%",
       }}
     >
+      <Typography variant="h6">{OPTIONS.TOURNAMENT_RULES_TITLE}</Typography>
+      <Typography variant="body1">{OPTIONS.EXPLAINATION_TOURNAMENT_RULES}</Typography>
       <CompendiumTableColOptions />
       <Divider sx={{ marginBottom: "2em" }} />
+      <Typography variant="h6">{OPTIONS.COMPENDIUM_ROWS_TITLE}</Typography> 
+      <Typography variant="body1">{OPTIONS.EXPLAINATION_COMPENDIUM_ROWS}</Typography>
       <LightSwitch bttnSize="medium" />
     </Grid>
   );
