573e7e3d39176f10b667ba6280eee6cdd393a891
marcelbade <62931394+marcelbade@users.noreply.github.com>
2025-09-01 23:33:45 +0200

    Refactored navigation button.

--- src/components/landingPage/NaviButton.js
+++ src/components/landingPage/NaviButton.js
@@ -1,17 +1,32 @@
 // React
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { useHistory } from "react-router-dom";
 // Material UI
-import { IconButton, Grid2 as Grid, Tooltip, Typography, Button } from "@mui/material";
-import { useTheme } from "@emotion/react";
+import { IconButton, Tooltip, Typography, Button } from "@mui/material";
 // Functions And Components
 import CustomIcon from "../shared/CustomIcon";
 // context
 import { MenuContext } from "../../contexts/MenuContext";
+// icons
+import { ChevronLeft } from "@mui/icons-material";
 
+
+/**
+ * This JSX component displays a navigation button. To account for all use cases, the
+ * button can be displayed as either icon or text button and can receive either mui
+ * icons or custom icons
+ * @param {props}
+ * - isIconButton: boolean
+ * - isCustomIcon: boolean
+ * - relativeURL: string
+ * - toolTipText: string
+ * - icon: any svg icon
+ * - altText: String
+ * - textButtonVariant: String
+ * @returns JSX
+ */
 const NaviButton = (props) => {
   const history = useHistory();
-  const theme = useTheme();
 
   const MC = useContext(MenuContext);
 
@@ -28,65 +43,50 @@ const NaviButton = (props) => {
     });
   };
 
-  const renderChild = (icon) => {
-    const Icon = icon;
-    return <Icon fontSize="large" />;
-  };
-
-  return (
-    <Grid
-      sx={{
-        width: props.width,
-        height: props.height,
-        textAlign: "center",
-      }}
-    >
-      {props.isIconButton ? (
-        <Tooltip title={<Typography sx={{ fontSize: "20px" }}>{props.text}</Typography>}>
-          <IconButton
-            onClick={() => {
-              toPage(props.relativeURL);
-              MC.setOpenMenu(false);
-            }}
-          >
-            {props.isCustomIcon ? (
-              <CustomIcon
-                icon={props.icon} //
-                altText={props.text}
-                height={HEIGHT_WIDTH}
-                width={HEIGHT_WIDTH}
-                boxHeight={HEIGHT_WIDTH_BOX}
-                boxWidth={HEIGHT_WIDTH_BOX}
-              />
-            ) : (
-              renderChild(props.icon)
-            )}
-          </IconButton>
-        </Tooltip>
-      ) : (
-        <Button
-          variant={props.variant}
-          onClick={() => {
-            toPage(props.relativeURL);
-            MC.setOpenMenu(false);
-          }}
-        >
-          {props.text}
-        </Button>
-      )}
-      <Typography
-        sx={{
-          fontSize: "15px",
-          [theme.breakpoints.up("md")]: {
-            display: "none",
-          },
-          [theme.breakpoints.down("lg")]: {},
+  return props.isIconButton ? (
+    <Tooltip title={<Typography sx={{ fontSize: "20px" }}>{props.toolTipText}</Typography>}>
+      <IconButton
+        onClick={() => {
+          toPage(props.relativeURL);
+          MC.setOpenMenu(false);
         }}
       >
-        {props.text}
-      </Typography>
-    </Grid>
+        {props.isCustomIcon ? (
+          <CustomIcon
+            icon={props.icon} //
+            altText={props.altText}
+            height={HEIGHT_WIDTH}
+            width={HEIGHT_WIDTH}
+            boxHeight={HEIGHT_WIDTH_BOX}
+            boxWidth={HEIGHT_WIDTH_BOX}
+          />
+        ) : (
+          <ChevronLeft sx={{ fontSize: "50px" }} />
+        )}
+      </IconButton>
+    </Tooltip>
+  ) : (
+    <Button
+      variant={props.textButtonVariant}
+      onClick={() => {
+        toPage(props.relativeURL);
+        MC.setOpenMenu(false);
+      }}
+    >
+      {props.text}
+    </Button>
   );
+  // <Typography
+  //   sx={{
+  //     fontSize: "15px",
+  //     [theme.breakpoints.up("md")]: {
+  //       display: "none",
+  //     },
+  //     [theme.breakpoints.down("lg")]: {},
+  //   }}
+  // >
+  //   {props.text}
+  // </Typography>
 };
 
 export default NaviButton;
