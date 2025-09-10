53404335f524dd338949ce8ed2f04886457673e9
marcelbade <62931394+marcelbade@users.noreply.github.com>
2025-09-10 16:11:01 +0200

    Custom dialog title added.

---
+++ src/components/Dialogs/components/CustomDialogTitle.js
@@ -0,0 +1,32 @@
+//  material ui
+import { DialogTitle, Grid2 as Grid, IconButton, useTheme } from "@mui/material";
+// functions and components
+import ContextHelpButton from "../../shared/ContextHelpButton";
+// icon
+import CancelIcon from "@mui/icons-material/Cancel";
+
+const CustomDialogTitle = (props) => {
+  const theme = useTheme();
+
+  return (
+    <Grid
+      container //
+      justifyContent="center"
+      sx={theme.palette.dialogs.title}
+    >
+      <DialogTitle>{props.title}</DialogTitle>
+      <ContextHelpButton
+        isVisible={props.displayHelpBttn}
+        message={props.pushMessage} //
+        type={props.pushMessageType}
+      />
+      <IconButton
+        onClick={props.handleClose} //
+      >
+        <CancelIcon color="error" />
+      </IconButton>
+    </Grid>
+  );
+};
+
+export default CustomDialogTitle;
