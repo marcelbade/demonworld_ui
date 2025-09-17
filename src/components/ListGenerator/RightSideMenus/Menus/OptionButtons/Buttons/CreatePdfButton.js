5cf8ddb1fca7048c01167cded59c2ee44b50276d
marcelbade <62931394+marcelbade@users.noreply.github.com>
2025-09-17 17:53:55 +0200

    Deleted import, rewrote fixed comments.

--- src/components/ListGenerator/RightSideMenus/Menus/OptionButtons/Buttons/CreatePdfButton.js
+++ src/components/ListGenerator/RightSideMenus/Menus/OptionButtons/Buttons/CreatePdfButton.js
@@ -1,7 +1,7 @@
 // react
 import { useContext, useState } from "react";
 // Material UI
-import { IconButton, Tooltip, Typography } from "@mui/material";
+import { IconButton, Tooltip } from "@mui/material";
 // components and functions
 import useSubFactionStats from "../../../../../../customHooks/UseSubFactionStats";
 import calculateScoutingFactor from "../../../../../../gameLogic/scoutFactorCalculator/scoutingFactorCalculator";
