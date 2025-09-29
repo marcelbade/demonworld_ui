643b6edd23b41924c2af7b0d69ff9d4bd83918d6
marcelbade <62931394+marcelbade@users.noreply.github.com>
2025-09-29 15:37:49 +0200

    Debugged regex issue.

--- src/components/Login/PasswordCriteriaCheck.js
+++ src/components/Login/PasswordCriteriaCheck.js
@@ -1,11 +1,13 @@
-/**
- * Every password in the app must pass the following criteria:
- * - at least 8 characters long
- * - contain letters, at least one number and one special character
- * - cotain at least one capitalized letter
- */
 
-const passwordRegEx = new RegExp(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/);
+// This regex will enforce these rules:
+//     At least one upper case English letter, (?=.*?[A-Z])
+//     At least one lower case English letter, (?=.*?[a-z])
+//     At least one digit, (?=.*?[0-9])
+//     At least one special character, (?=.*?[#?!@$%^&*-])
+//     Minimum eight in length .{8,} (with the anchors)
+
+  
+const passwordRegEx = new RegExp(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/);
 
 /**
  * Function tests via RegEx if the password is valid.
