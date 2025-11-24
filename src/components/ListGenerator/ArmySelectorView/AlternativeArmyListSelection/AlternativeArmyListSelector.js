e620b6638069ee77335fe1da457f0170b0e152c8
marcelbade <62931394+marcelbade@users.noreply.github.com>
2025-11-24 21:40:02 +0100

    massive rewrite of the AlternativeArmyListSelector, mostly functions for orks as well as missing comments.

--- src/components/ListGenerator/ArmySelectorView/AlternativeArmyListSelection/AlternativeArmyListSelector.js
+++ src/components/ListGenerator/ArmySelectorView/AlternativeArmyListSelection/AlternativeArmyListSelector.js
@@ -2,146 +2,130 @@
 import { useContext, useState } from "react";
 // components and functions
 import SelectionInput from "../../../shared/selectionInput";
-import useAlternativeFactionRules from "../../../../customHooks/UseAlternativeFactionRules";
 // context
 import { ArmyContext } from "../../../../contexts/armyContext";
 import { AlternativeListContext } from "../../../../contexts/alternativeListContext";
 // constants
 import { ALTERNATIVE_ARMY_SELECTION_TEXT, NONE } from "../../../../constants/factions";
+import { DWARF_TEXTS, EMPIRE_TEXTS, ORK_CLANS_TEXTS } from "../../../../constants/textsAndMessages";
 
 const AlternativeArmyListSelector = () => {
   const AC = useContext(ArmyContext);
   const ALC = useContext(AlternativeListContext);
 
-  // create an array that has many elements as there are choices to make and give each element "" as default value.
-  const [selectionArray, setSelectionArray] = useState(Array(ALC.numberOfAlternativeChoices).fill(""));
-
-  const OPTIONS = useAlternativeFactionRules(AC.selectedFactionName, ALC.alternateListNames);
+  const [currentAlternativeLists, setCurrentAlternativeLists] = useState([]);
 
   /**
-   * Filter function for the Selection Inputs. Assigns a value
-   * to the element of the selectedAlternativeLists that corresponds
-   * to te selector Number.
-   * @param {Event.target.value} value
-   * @param {integer} selectorNumber
+   * Function is the onChange function for the SelectionInput.
+   * Lets user select and set alternative armies for the factions that have them.
+   * @param {String} value
+   * @param {integer} iterator
    */
-  const selectAlternateList = (value, selectorNumber) => {
-    let tempArray = [...selectionArray];
-    tempArray[selectorNumber] = value;
+  const selectOnInputChange = (value, iterator) => {
+    ALC.setSelectedAlternativeLists([...ALC.selectedAlternativeLists, value]);
 
-    setSelectionArray(tempArray);
-    setAlternatives(selectorNumber);
-    isSelectionComplete(tempArray);
-  };
+    switch (AC.selectedFactionName) {
+      case ORK_CLANS_TEXTS.FACTION_NAME:
+        setOrkAlternatives(value, iterator);
+        break;
+      case EMPIRE_TEXTS.FACTION_NAME:
+        setEmpireAlternatives(value, iterator);
+        break;
 
-  /**
-   * Clear function for the Selection Inputs. Removes
-   * the value from selectedAlternativeLists element that corresponds
-   * to the selectorNumber
-   * @param {int} selectorNumber
-   */
-  const clearAlternateList = (selectorNumber) => {
-    let tempArray = [...selectionArray];
-    tempArray[selectorNumber] = "";
+      case DWARF_TEXTS.FACTION_NAME:
+        setDwarvesAlternatives(value, iterator);
+        break;
 
-    setSelectionArray(tempArray);
-    setAlternatives(selectorNumber);
-    isSelectionComplete(tempArray);
-  };
-
-  /**
-   * Function sets the options that are displayed in the selection input's dropdown menu.
-   * Allready selected values are filtered out.
-   * @returns an array of String values to be displayed.
-   */
-  const setAlternatives = (selectorNumber) => {
-    if (OPTIONS === undefined) {
-      return;
+      default:
+        throw new Error("AlternativeArmyListSelector: could not find faction to set alternative list options.");
     }
+  };
 
-    if (OPTIONS[selectorNumber] === undefined) {
-      return;
+  const setOrkAlternatives = (clanName, iterator) => {
+    let tempArray = [...currentAlternativeLists];
+    tempArray.push(clanName);
+
+    setCurrentAlternativeLists([...tempArray]);
+
+    if (iterator === 1) {
+      markChoicesAndCloseSelector(tempArray);
     }
-
-    return OPTIONS[selectorNumber].filter((o) => !selectionArray.includes(o));
   };
 
-  /**
-   * Function tests if the selection is complete. This is the case
-   * if the number of selected values is equal to the number of input elements.
-   * If complete, the army selection tree is displayed in the UI.
-   * @param {[String]} the current value of the selectionArray.
-   */
-  const isSelectionComplete = (tempArray) => {
-    const elementsFilled = tempArray.filter((e) => e !== "").length;
-    const isComplete = elementsFilled === ALC.numberOfAlternativeChoices;
+  const markChoicesAndCloseSelector = (selectedAlternativeSubFactions) => {
+    let tempArray = [...AC.subFactionDTOs];
 
-    setGlobalState(isComplete, tempArray);
-    setSelectedAlternateSubFaction(tempArray);
-  };
-
-  /**
-   * Function sets the relevant fields of the global state
-   * once the selection is complete.
-   * @param {Boolean} isComplete
-   * @param {[String]} tempArray
-   */
-  const setGlobalState = (isComplete, tempArray) => {
-    ALC.setAltArmyListSelectionComplete(isComplete);
-    ALC.setSelectedAlternativeLists(tempArray);
-  };
-
-  /**
-   * Function sets the "selectedAlternativeOption" flag to true for
-   * those sub factions that have been selected.
-   * @param {[Strings]} tempArray
-   */
-  const setSelectedAlternateSubFaction = (tempArray) => {
-    const clonedSubFactionList = structuredClone(AC.subFactionDTOs);
-
-    clonedSubFactionList.forEach((dto) => {
-      dto.selectedAlternativeOption = tempArray.includes(dto.name);
+    tempArray.forEach((dto) => {
+      if (selectedAlternativeSubFactions.includes(dto.name)) {
+        dto.selectedAlternativeOption = true;
+      }
     });
 
-    AC.setSubFactionDTOs(clonedSubFactionList);
+    AC.setSubFactionDTOs([...tempArray]);
+    ALC.setAltArmyListSelectionComplete(true);
   };
 
   /**
-   * Function returns the text for the alternative army input field(s)
-   * @param {*} selectorNumber
-   * @returns a string with the selection text for the army and selector.
+   * Function sets the alternative sub factions for the empire faction.
+   * The user must choose two marches with the added condition that only adjacent
+   * marches can be selected.
+   * @param {*} marchName
+   * @param {*} iterator
    */
-  const setLabel = (selectorNumber) => {
+  const setEmpireAlternatives = (marchName, iterator) => {
+    const adjacentMarches = {
+      northAndSouth: ["Westmark", "Ostmark"],
+      eastAndWest: ["Nordmark", "Südmark"],
+    };
+
+    if ((iterator === 0 && marchName === EMPIRE_TEXTS.SF.NORTH_MARCH) || marchName === EMPIRE_TEXTS.SF.SOUTH_MARCH) {
+      setCurrentAlternativeLists(adjacentMarches.northAndSouth);
+    }
+    if ((iterator === 0 && marchName === EMPIRE_TEXTS.SF.WEST_MARCH) || marchName === EMPIRE_TEXTS.SF.EAST_MARCH) {
+      setCurrentAlternativeLists(adjacentMarches.eastAndWest);
+    }
+    if (iterator === 1) {
+      ALC.setAltArmyListSelectionComplete(true);
+    }
+  };
+
+  /**
+   *
+   */
+  const setDwarvesAlternatives = () => {};
+
+  const clearAlternateList = () => {
+    ALC.setSelectedAlternativeLists([]);
+  };
+
+  /**
+   * Function sets the text for the dropdown menu that let's the user
+   * select the alternative army lists.
+   * @param {integer} iterator
+   * @returns a String
+   */
+  const setLabel = (iterator) => {
     if (ALTERNATIVE_ARMY_SELECTION_TEXT[AC.selectedFactionName] === undefined) {
       return;
     }
-
-    return ALTERNATIVE_ARMY_SELECTION_TEXT[AC.selectedFactionName][selectorNumber];
+    return ALTERNATIVE_ARMY_SELECTION_TEXT[AC.selectedFactionName][iterator];
   };
 
-  /**
-   * Functinon tests whether the army has alternative lists.
-   * @returns true, if a fation has been selected and the army
-   * has alternative lists.
-   */
-  const areAlternativeListsPresent = () => {
-    return (
-      AC.selectedFactionName !== NONE && //
-      ALC.armyHasAlternativeLists
-    );
-  };
-
-  return areAlternativeListsPresent()
-    ? [...Array(ALC.numberOfAlternativeChoices).keys()].map((selectorNumber) => {
+  return AC.selectedFactionName !== NONE && ALC.armyHasAlternativeLists && !ALC.altArmyListSelectionComplete
+    ? [...Array(ALC.numberOfAlternativeChoices).keys()].map((iterator) => {
         return (
           <SelectionInput //
             width={"32em"}
-            key={selectorNumber}
-            selectorNumber={selectorNumber}
-            alternatives={setAlternatives(selectorNumber)}
-            filterFunction={selectAlternateList}
-            clearFunction={clearAlternateList}
-            label={setLabel(selectorNumber)}
+            key={iterator}
+            selectorNumber={iterator}
+            alternatives={ALC.alternateListNames}
+            filterFunction={(value) => {
+              selectOnInputChange(value, iterator);
+            }}
+            clearFunction={() => {
+              clearAlternateList();
+            }}
+            label={setLabel(iterator)}
           />
         );
       })
