37b610bdede3457a7620ad21648e5d405a6992ab
marcelbade <62931394+marcelbade@users.noreply.github.com>
2025-12-04 01:56:55 +0100

    Removed unused Context.

--- src/contexts/statCardContext.js
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-
-const StatCardContext = createContext();
-const StatCardProvider = StatCardContext.Provider;
-
-export { StatCardContext };
-export default StatCardProvider;
