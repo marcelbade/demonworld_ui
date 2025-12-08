9940b29ef0a2736151397dc087bfd70ed5a39f88
marcelbade <62931394+marcelbade@users.noreply.github.com>
2025-12-08 20:42:48 +0100

    Turned box into drawer to make it responsive.

--- src/components/ListGenerator/ArmySelectorView/ArmySelectionBox.js
+++ src/components/ListGenerator/ArmySelectorView/ArmySelectionBox.js
@@ -1,16 +1,47 @@
 // react
-import React from "react";
+import { useTheme } from "@emotion/react";
+// material ui
+import { Drawer, IconButton } from "@mui/material";
 // components and functions
 import AlternativeArmyListSelector from "./AlternativeArmyListSelection/AlternativeArmyListSelector";
 import FactionTreeView from "./SelectorTreeView/FactionTreeView";
-import { Box } from "@mui/material";
+// icons
+import CancelIcon from "@mui/icons-material/Cancel";
 
-const ArmySelectionBox = () => {
+const ArmySelectionBox = (props) => {
+  const theme = useTheme();
+
   return (
-    <Box>
+    <Drawer
+      anchor={"left"} //
+      variant="persistent"
+      open={props.openArmySelectionBox}
+      sx={{
+        "& .MuiPaper-root": {
+          paddingTop: "0.5em",
+          backgroundColor: theme.palette.contrastedOptions, //
+          position: "fixed",
+          overflowY: "auto",
+          width: "28em",
+        },
+      }}
+    >
+      <span>
+        <IconButton
+          onClick={props.toggleUnitTree}
+          fontSize="large"
+          sx={{
+            marginTop: "0.5em",
+            marginBottom: "0.5em",
+            marginLeft:"16em"
+          }}
+        >
+          <CancelIcon />
+        </IconButton>
+      </span>
       <AlternativeArmyListSelector />
       <FactionTreeView />
-    </Box>
+    </Drawer>
   );
 };
 
