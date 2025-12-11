3008d3a766373f34432bc6b67d9e45ce48e17369
marcelbade <62931394+marcelbade@users.noreply.github.com>
2025-12-11 13:45:56 +0100

    Appended useCostumMedia hook.

--- src/customHooks/UseCustomMediaQuery.js
+++ src/customHooks/UseCustomMediaQuery.js
@@ -2,22 +2,26 @@ import { useTheme } from "@emotion/react";
 import { useMediaQuery } from "@mui/material";
 
 /**
- * Custom hook uses useMediaQuery to return several functions that allow
- * the conditional rendering of components and variants. Each function checks
- * whether the display is a certain size and returns true, if that is the case.
+ * Custom hook uses useMediaQuery to return functions that allow
+ * the conditional rendering of components.   
  * @returns an object with the following functions:
  * - isLargeDisplay
  * - isMediumDisplay
  * - isSmallDisplay
  * - isTinyDisplay
+   - isPortraitModus
+ * - isLandscapeModus
  */
 const useCustomMediaQuery = () => {
   const theme = useTheme();
+
   return {
     isLargeDisplay: useMediaQuery(theme.breakpoints.down("xl")),
     isMediumDisplay: useMediaQuery(theme.breakpoints.down("lg")),
     isSmallDisplay: useMediaQuery(theme.breakpoints.down("md")),
     isTinyDisplay: useMediaQuery(theme.breakpoints.down("sm")),
+    isPortraitModus: window.matchMedia("(orientation:portrait)").matches,
+    isLandscapeModus: window.matchMedia("(orientation:landscape)").matches,
   };
 };
 
