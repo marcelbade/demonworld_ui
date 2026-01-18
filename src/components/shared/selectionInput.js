49982fc85b4e0e406ea830385f9a59bf4e2d87e2
marcelbade <62931394+marcelbade@users.noreply.github.com>
2026-01-18 16:17:14 +0100

    Improved formatting.

--- src/components/shared/selectionInput.js
+++ src/components/shared/selectionInput.js
@@ -13,17 +13,35 @@ const SelectionInput = (props) => {
       multiple={props.allowsMultiple}
       disabled={props.disabled === undefined ? false : props.disabled}
       sx={{
-        color: props.textColor === null ? "theme.palette.color" : props.textColor,
+        color:
+          props.textColor === null //
+            ? "theme.palette.color"
+            : props.textColor,
+
         marginLeft: "2em",
-        width: props.width === undefined ? null : props.width,
+
+        width:
+          props.width === undefined //
+            ? null
+            : props.width,
+
         "& .MuiInput-input": {
-          color: props.textColor === null ? "theme.palette.color" : props.textColor,
+          color:
+            props.textColor === null //
+              ? "theme.palette.color"
+              : props.textColor,
         },
 
         "& .MuiFormLabel-root": {
           fontFamily: "NotMaryKate",
-          color: props.textColor === null ? "theme.palette.color" : props.textColor,
-          fontSize: props.isArmySelector ? "30px" : "15px",
+          color:
+            props.textColor === null //
+              ? "theme.palette.color"
+              : props.textColor,
+              
+          fontSize: props.isArmySelector //
+            ? "30px"
+            : "15px",
         },
       }}
       id="arymSelection"
